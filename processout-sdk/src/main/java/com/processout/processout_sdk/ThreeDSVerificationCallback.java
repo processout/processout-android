4004f3c7cbf04e1d9a0018a9161622d5424fe231
Jeremy Lejoux <jeremy@processout.com>
2019-08-26 17:53:07 +0200

    threeds: optimize imports

--- processout-sdk/src/main/java/com/processout/processout_sdk/ThreeDSVerificationCallback.java
+++ processout-sdk/src/main/java/com/processout/processout_sdk/ThreeDSVerificationCallback.java
@@ -1,7 +1,5 @@
 package com.processout.processout_sdk;
 
-import com.processout.processout_sdk.ProcessOutExceptions.ProcessOutException;
-
 public interface ThreeDSVerificationCallback {
     public void onSuccess(String invoiceId);
     public void onError(Exception error);
