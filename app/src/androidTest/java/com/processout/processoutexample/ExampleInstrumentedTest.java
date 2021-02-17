e32047b1ab2426c8315c7512cd5135abb57c8f20
Adam Szewera <adam.szewe@gmail.com>
2021-02-17 11:07:59 +0100

    Fix the example instrumentation test
    Fix the example instrumentation test and change the runner to AndroidX AndroidJUnitRunner.

--- app/src/androidTest/java/com/processout/processoutexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/processout/processoutexample/ExampleInstrumentedTest.java
@@ -5,11 +5,10 @@ import android.content.Context;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -21,8 +20,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = ApplicationProvider.getApplicationContext();
-
+        Context appContext = appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.processout.processoutexample", appContext.getPackageName());
     }
 }
