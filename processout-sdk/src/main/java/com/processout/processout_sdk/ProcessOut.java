5baf915036e09dd48a1cb3f959ff0985bca99bee
Patricia <patricia.cabiles@checkout.com>
2022-04-13 12:00:05 +0100

    add thirdpartysdk version to incremental auth

--- processout-sdk/src/main/java/com/processout/processout_sdk/ProcessOut.java
+++ processout-sdk/src/main/java/com/processout/processout_sdk/ProcessOut.java
@@ -392,6 +392,53 @@ public class ProcessOut {
     }
 
     /**
+     * Allow card payments authorization and marks the authorization as incremental (with 3DS2 support)
+     *
+     * @param invoiceId previously generated invoice
+     * @param source    source to use for the charge (card token, etc.)
+     * @param thirdPartySDKVersion version of the 3rd party SDK being used for the calls.
+     * @param handler   (Custom 3DS2 handler)
+     */
+    public void makeIncrementalAuthorizationPayment(@NonNull final String invoiceId, @NonNull final String source, final String thirdPartySDKVersion, @NonNull final ThreeDSHandler handler, @NonNull final Context with) {
+        try {
+            // Generate the authorization body and forces 3DS2
+            AuthorizationRequest authRequest = new AuthorizationRequest(source, true, thirdPartySDKVersion);
+            final JSONObject body = new JSONObject(gson.toJson(authRequest));
+
+            requestAuthorization(invoiceId, body, new RequestAuthorizationCallback() {
+                @Override
+                public void onError(Exception error) {
+                    handler.onError(error);
+                }
+
+                @Override
+                public void onSuccess(JSONObject json) {
+                    // Handle the authorization result
+                    AuthorizationResult result = gson.fromJson(
+                            json.toString(), AuthorizationResult.class);
+
+                    CustomerAction cA = result.getCustomerAction();
+                    if (cA == null) {
+                        // No customer action in the authorization result, we return the invoice id
+                        handler.onSuccess(invoiceId);
+                        return;
+                    }
+
+                    CustomerActionHandler customerActionHandler = new CustomerActionHandler(handler, new PaymentWebView(with), with, new CustomerActionHandler.CustomerActionCallback() {
+                        @Override
+                        public void shouldContinue(String source) {
+                            makeIncrementalAuthorizationPayment(invoiceId, source, handler, with);
+                        }
+                    });
+                    customerActionHandler.handleCustomerAction(cA);
+                }
+            });
+        } catch (JSONException e) {
+            handler.onError(e);
+        }
+    }
+
+    /**
      * Increments the authorization of an applicable invoice by a given amount
      *
      * @param invoiceId previously generated invoice
