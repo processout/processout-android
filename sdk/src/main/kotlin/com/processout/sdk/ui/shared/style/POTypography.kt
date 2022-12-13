f5d27ee7edb4c51b350236b2f35efa81feb01319
Vitalii Vanziak <114916876+vitalii-vanziak-cko@users.noreply.github.com>
2022-12-13 12:34:42 +0200

    feat(POM-88): Typography (#19)

--- sdk/src/main/kotlin/com/processout/sdk/ui/shared/style/POTypography.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/shared/style/POTypography.kt
@@ -7,10 +7,45 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class POTypography(
     @FontRes
-    val fontResId: Int? = null
+    val fontResId: Int? = null,
+    val weight: Weight,
+    val italic: Boolean = false,
+    val textSizeSp: Int,
+    val lineHeightSp: Int
 ) : Parcelable {
 
+    @Parcelize
+    enum class Weight(val value: Int) : Parcelable {
+        THIN(100),
+        EXTRA_LIGHT(200),
+        LIGHT(300),
+        NORMAL(400),
+        MEDIUM(500),
+        SEMI_BOLD(600),
+        BOLD(700),
+        EXTRA_BOLD(800),
+        BLACK(900)
+    }
+
     companion object {
-        val default = POTypography()
+        val titleLarge = POTypography(weight = Weight.NORMAL, textSizeSp = 24, lineHeightSp = 32)
+
+        val titleDefault = POTypography(weight = Weight.NORMAL, textSizeSp = 20, lineHeightSp = 24)
+
+        val bodyDefault = POTypography(weight = Weight.NORMAL, textSizeSp = 16, lineHeightSp = 24)
+
+        val bodyDefaultMedium = POTypography(weight = Weight.MEDIUM, textSizeSp = 16, lineHeightSp = 24)
+
+        val bodySmall = POTypography(weight = Weight.NORMAL, textSizeSp = 12, lineHeightSp = 16)
+
+        val bodySmallMedium = POTypography(weight = Weight.MEDIUM, textSizeSp = 12, lineHeightSp = 16)
+
+        val inputLabel = POTypography(weight = Weight.NORMAL, textSizeSp = 16, lineHeightSp = 24)
+
+        val inputString = POTypography(weight = Weight.NORMAL, textSizeSp = 16, lineHeightSp = 24)
+
+        val actionDefault = POTypography(weight = Weight.NORMAL, textSizeSp = 16, lineHeightSp = 24)
+
+        val actionDefaultMedium = POTypography(weight = Weight.MEDIUM, textSizeSp = 16, lineHeightSp = 24)
     }
 }
