8291a2a1db8977e910c74b64bf363b304e232b7b
Vitalii Vanziak <vitalii.vanziak@processout.com>
2023-04-27 18:12:53 +0300

    Default dropdown styles

--- sdk/src/main/kotlin/com/processout/sdk/ui/shared/view/input/dropdown/ExposedDropdownInput.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/shared/view/input/dropdown/ExposedDropdownInput.kt
@@ -1,18 +1,23 @@
 package com.processout.sdk.ui.shared.view.input.dropdown
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.google.android.material.textfield.TextInputLayout
 import com.processout.sdk.R
 import com.processout.sdk.api.model.response.PONativeAlternativePaymentMethodParameter.ParameterValue
 import com.processout.sdk.ui.shared.model.InputParameter
 import com.processout.sdk.ui.shared.style.input.POInputStyle
+import com.processout.sdk.ui.shared.view.extensions.defaultOutlineBackground
 import com.processout.sdk.ui.shared.view.extensions.hideKeyboard
+import com.processout.sdk.ui.shared.view.extensions.outlineBackground
 import com.processout.sdk.ui.shared.view.input.Input
 import com.processout.sdk.ui.shared.view.input.InputComponent
 
@@ -29,8 +34,19 @@ internal class ExposedDropdownInput(
     private var state: Input.State = inputParameter?.state ?: Input.State.Default()
 
     private val title: TextView
+    private val dropdownLayout: TextInputLayout
     private val dropdownAutoComplete: MaterialAutoCompleteTextView
     private val errorMessage: TextView
+
+    private var defaultBackground = defaultOutlineBackground(context, R.color.poBorderPrimary)
+    private var errorBackground = defaultOutlineBackground(context, R.color.poBorderError)
+
+    private var defaultDropdownBackground = outlineBackground(
+        cornerRadiusPx = context.resources.getDimensionPixelSize(R.dimen.po_cornerRadius).toFloat(),
+        borderWidthPx = 0,
+        borderColor = Color.TRANSPARENT,
+        backgroundColor = ContextCompat.getColor(context, R.color.poBackgroundPrimary)
+    )
 
     private var adapter: ParameterValueAdapter? = null
     private var afterValueChanged: ((String) -> Unit)? = null
@@ -50,8 +66,11 @@ internal class ExposedDropdownInput(
         orientation = VERTICAL
 
         title = findViewById(R.id.po_title)
+        dropdownLayout = findViewById(R.id.po_dropdown_layout)
         dropdownAutoComplete = findViewById(R.id.po_dropdown_auto_complete)
         errorMessage = findViewById(R.id.po_error_message)
+
+        dropdownAutoComplete.setDropDownBackgroundDrawable(defaultDropdownBackground)
 
         setListeners()
         initWithInputParameters()
@@ -114,11 +133,13 @@ internal class ExposedDropdownInput(
         when (state) {
             is Input.State.Default -> {
                 dropdownAutoComplete.isEnabled = state.editable
+                dropdownLayout.background = defaultBackground
                 errorMessage.text = String()
                 errorMessage.visibility = View.INVISIBLE
             }
             is Input.State.Error -> {
                 dropdownAutoComplete.isEnabled = true
+                dropdownLayout.background = errorBackground
                 errorMessage.text = state.message
                 errorMessage.visibility = View.VISIBLE
             }
