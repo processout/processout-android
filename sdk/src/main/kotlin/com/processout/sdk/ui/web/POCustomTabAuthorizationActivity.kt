a9465efabefd19fff5ea62cf463aee8f530df310
Vitalii Vanziak <vitalii.vanziak@processout.com>
2023-05-31 16:27:28 +0300

    Custom Tab redirect and theme

--- sdk/src/main/kotlin/com/processout/sdk/ui/web/POCustomTabAuthorizationActivity.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/web/POCustomTabAuthorizationActivity.kt
@@ -1,7 +1,39 @@
 package com.processout.sdk.ui.web
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
+import com.processout.sdk.api.model.threeds.PO3DSRedirect
+import com.processout.sdk.ui.threeds.CustomTab3DSAuthorizationActivityContract
 
 class POCustomTabAuthorizationActivity : AppCompatActivity() {
 
+    private var redirect: PO3DSRedirect? = null
+
+    @Suppress("DEPRECATION")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        redirect = intent.getParcelableExtra(
+            CustomTab3DSAuthorizationActivityContract.EXTRA_CONFIGURATION
+        )
+        redirect?.let {
+            val intent: CustomTabsIntent = CustomTabsIntent.Builder()
+                .setShareState(CustomTabsIntent.SHARE_STATE_OFF)
+                .build()
+            intent.launchUrl(this, Uri.parse(it.url.toString()))
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        // TODO: handle activity result
+        finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // TODO: handle cancel
+    }
 }
--- sdk/src/main/kotlin/com/processout/sdk/ui/web/POCustomTabRedirectActivity.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/web/POCustomTabRedirectActivity.kt
@@ -1,7 +1,18 @@
 package com.processout.sdk.ui.web
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class POCustomTabRedirectActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Intent(this, POCustomTabAuthorizationActivity::class.java).let {
+            it.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            intent.data?.let { uri -> it.data = uri }
+            startActivity(it)
+        }
+        finish()
+    }
 }
