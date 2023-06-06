90227e70f72a7942fd7be0360a5d49ef228d11cf
Vitalii Vanziak <vitalii.vanziak@processout.com>
2023-06-06 23:27:48 +0300

    POWebViewAuthorizationActivity & screen orientation fixes

--- sdk/src/main/kotlin/com/processout/sdk/ui/web/customtab/POCustomTabAuthorizationActivity.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/web/customtab/POCustomTabAuthorizationActivity.kt
@@ -1,6 +1,8 @@
 package com.processout.sdk.ui.web.customtab
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
@@ -14,8 +16,13 @@ class POCustomTabAuthorizationActivity : AppCompatActivity() {
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        configuration = intent.getParcelableExtra(EXTRA_CONFIGURATION)
+        // This is a workaround for crash on Android 8.0 (API level 26).
+        // https://issuetracker.google.com/issues/68454482
+        // https://stackoverflow.com/questions/48072438/java-lang-illegalstateexception-only-fullscreen-opaque-activities-can-request-o
+        requestedOrientation = if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O)
+            ActivityInfo.SCREEN_ORIENTATION_BEHIND else ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
+        configuration = intent.getParcelableExtra(EXTRA_CONFIGURATION)
         configuration?.let {
             val customTabsIntent: CustomTabsIntent = CustomTabsIntent.Builder()
                 .setShareState(CustomTabsIntent.SHARE_STATE_OFF)
--- sdk/src/main/kotlin/com/processout/sdk/ui/web/webview/POWebViewAuthorizationActivity.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/web/webview/POWebViewAuthorizationActivity.kt
@@ -1,22 +1,59 @@
 package com.processout.sdk.ui.web.webview
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
+import com.processout.sdk.core.POFailure
+import com.processout.sdk.core.ProcessOutActivityResult
+import com.processout.sdk.core.toActivityResult
 import com.processout.sdk.ui.web.webview.WebViewAuthorizationActivityContract.Companion.EXTRA_CONFIGURATION
+import com.processout.sdk.ui.web.webview.WebViewAuthorizationActivityContract.Companion.EXTRA_RESULT
 
 class POWebViewAuthorizationActivity : AppCompatActivity() {
-
-    private var configuration: WebViewConfiguration? = null
 
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        configuration = intent.getParcelableExtra(EXTRA_CONFIGURATION)
-        configuration?.let { configuration ->
-            val webView = ProcessOutWebView(this, configuration) { result ->
-                // TODO
+        // This is a workaround for crash on Android 8.0 (API level 26).
+        // https://issuetracker.google.com/issues/68454482
+        // https://stackoverflow.com/questions/48072438/java-lang-illegalstateexception-only-fullscreen-opaque-activities-can-request-o
+        requestedOrientation = if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O)
+            ActivityInfo.SCREEN_ORIENTATION_BEHIND else ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        dispatchBackPressed()
+        intent.getParcelableExtra<WebViewConfiguration>(EXTRA_CONFIGURATION)
+            ?.let { configuration ->
+                setContentView(ProcessOutWebView(this, configuration) {
+                    finishWithActivityResult(it.toActivityResult())
+                })
             }
-            setContentView(webView)
+    }
+
+    private fun dispatchBackPressed() {
+        onBackPressedDispatcher.addCallback(this) {
+            finishWithActivityResult(
+                ProcessOutActivityResult.Failure(
+                    POFailure.Code.Cancelled,
+                    "Cancelled by user with back press or gesture."
+                )
+            )
         }
+    }
+
+    private fun finishWithActivityResult(result: ProcessOutActivityResult<Uri>) {
+        when (result) {
+            is ProcessOutActivityResult.Success -> setActivityResult(Activity.RESULT_OK, result)
+            is ProcessOutActivityResult.Failure -> setActivityResult(Activity.RESULT_CANCELED, result)
+        }
+        finish()
+    }
+
+    private fun setActivityResult(resultCode: Int, result: ProcessOutActivityResult<Uri>) {
+        setResult(resultCode, Intent().putExtra(EXTRA_RESULT, result))
     }
 }
