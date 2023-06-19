51ce22401f38cc023d05b106e3d54d3523214c2a
Vitalii Vanziak <vitalii.vanziak@processout.com>
2023-06-19 15:33:47 +0300

    POBackgroundStyle

--- sdk/src/main/kotlin/com/processout/sdk/ui/nativeapm/PONativeAlternativePaymentMethodConfiguration.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/nativeapm/PONativeAlternativePaymentMethodConfiguration.kt
@@ -3,6 +3,7 @@ package com.processout.sdk.ui.nativeapm
 import android.os.Parcelable
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
+import com.processout.sdk.ui.shared.style.POBackgroundStyle
 import com.processout.sdk.ui.shared.style.POTextStyle
 import com.processout.sdk.ui.shared.style.background.POBackgroundDecorationStyle
 import com.processout.sdk.ui.shared.style.button.POButtonStyle
@@ -72,7 +73,9 @@ data class PONativeAlternativePaymentMethodConfiguration(
         val radioButton: PORadioButtonStyle? = null,
         val primaryButton: POButtonStyle? = null,
         val secondaryButton: POButtonStyle? = null,
+        val background: POBackgroundStyle? = null,
         @ColorInt
+        @Deprecated("Use property 'background: POBackgroundStyle'.")
         val backgroundColor: Int? = null,
         @ColorInt
         val progressIndicatorColor: Int? = null,
@@ -80,6 +83,7 @@ data class PONativeAlternativePaymentMethodConfiguration(
         val successMessage: POTextStyle? = null,
         @DrawableRes
         val successImageResId: Int? = null,
+        @Deprecated("Use property 'background: POBackgroundStyle'.")
         val backgroundDecoration: POBackgroundDecorationStyle? = null
     ) : Parcelable
 }
---
+++ sdk/src/main/kotlin/com/processout/sdk/ui/shared/style/POBackgroundStyle.kt
@@ -0,0 +1,13 @@
+package com.processout.sdk.ui.shared.style
+
+import android.os.Parcelable
+import androidx.annotation.ColorInt
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class POBackgroundStyle(
+    @ColorInt
+    val normal: Int,
+    @ColorInt
+    val success: Int
+) : Parcelable
