186365e3a29f1055fe40cbc807ecb6d77b82f102
Vitalii Vanziak <114916876+vitalii-vanziak-cko@users.noreply.github.com>
2023-06-21 14:52:57 +0300

    feat(POM-218): Allow getting card co/scheme via SDK (#82)

--- sdk/src/main/kotlin/com/processout/sdk/api/network/CardsApi.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/network/CardsApi.kt
@@ -2,9 +2,11 @@ package com.processout.sdk.api.network
 
 import com.processout.sdk.api.model.request.POCardTokenizationRequestWithDeviceData
 import com.processout.sdk.api.model.request.POCardUpdateCVCRequest
+import com.processout.sdk.api.model.response.POCardIssuerInformationResponse
 import com.processout.sdk.api.model.response.POCardResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -12,11 +14,18 @@ import retrofit2.http.Path
 internal interface CardsApi {
 
     @POST("/cards")
-    suspend fun tokenize(@Body request: POCardTokenizationRequestWithDeviceData): Response<POCardResponse>
+    suspend fun tokenize(
+        @Body request: POCardTokenizationRequestWithDeviceData
+    ): Response<POCardResponse>
 
     @PUT("/cards/{id}")
     suspend fun updateCVC(
         @Path("id") cardId: String,
         @Body request: POCardUpdateCVCRequest
     ): Response<POCardResponse>
+
+    @GET("/iins/{iin}")
+    suspend fun fetchIssuerInformation(
+        @Path("iin") iin: String
+    ): Response<POCardIssuerInformationResponse>
 }
--- sdk/src/main/kotlin/com/processout/sdk/api/repository/POCardsRepository.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/repository/POCardsRepository.kt
@@ -3,6 +3,7 @@ package com.processout.sdk.api.repository
 import com.processout.sdk.api.model.request.POCardTokenizationRequest
 import com.processout.sdk.api.model.request.POCardUpdateCVCRequest
 import com.processout.sdk.api.model.response.POCard
+import com.processout.sdk.api.model.response.POCardIssuerInformation
 import com.processout.sdk.core.ProcessOutCallback
 import com.processout.sdk.core.ProcessOutResult
 
@@ -19,5 +20,20 @@ interface POCardsRepository {
         cardId: String,
         request: POCardUpdateCVCRequest,
         callback: ProcessOutCallback<POCard>
+    )
+
+    /**
+     * Allows to fetch card issuer information based on iin.
+     * @param iin Card issuer identification number. Corresponds to the first 6 or 8 digits of the main card number.
+     */
+    suspend fun fetchIssuerInformation(iin: String): ProcessOutResult<POCardIssuerInformation>
+
+    /**
+     * Allows to fetch card issuer information based on iin.
+     * @param iin Card issuer identification number. Corresponds to the first 6 or 8 digits of the main card number.
+     */
+    fun fetchIssuerInformation(
+        iin: String,
+        callback: ProcessOutCallback<POCardIssuerInformation>
     )
 }
