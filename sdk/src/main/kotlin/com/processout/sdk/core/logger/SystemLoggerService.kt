6cfa8fc771b66b118500be5ade793b972a8b12fa
Vitalii Vanziak <vitalii.vanziak@processout.com>
2023-08-17 18:39:54 +0300

    Simplified SystemLoggerService

--- sdk/src/main/kotlin/com/processout/sdk/core/logger/SystemLoggerService.kt
+++ sdk/src/main/kotlin/com/processout/sdk/core/logger/SystemLoggerService.kt
@@ -12,18 +12,8 @@ internal class SystemLoggerService(
 
     override fun log(event: LogEvent) {
         val message = "${event.message} ${event.attributes}"
-        // Split by line and ensure each line can fit into maximum length.
-        var i = 0
-        val length = message.length
-        while (i < length) {
-            var newline = message.indexOf('\n', i)
-            newline = if (newline != -1) newline else length
-            do {
-                val end = minOf(newline, i + MAX_LOG_LENGTH)
-                Log.println(event.level.priority, event.tag, message.substring(i, end))
-                i = end
-            } while (i < newline)
-            i++
+        message.chunked(MAX_LOG_LENGTH).forEach {
+            Log.println(event.level.priority, event.tag, it)
         }
     }
 }
