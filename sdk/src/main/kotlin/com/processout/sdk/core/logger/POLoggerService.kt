cbafd6a08ff7c68f3653a1298b154a5f885dab73
Vitalii Vanziak <vitalii.vanziak@processout.com>
2023-08-23 22:09:06 +0300

    Added ProcessOutInternalApi annotation for POLoggerService log method

--- sdk/src/main/kotlin/com/processout/sdk/core/logger/POLoggerService.kt
+++ sdk/src/main/kotlin/com/processout/sdk/core/logger/POLoggerService.kt
@@ -5,6 +5,7 @@ import com.processout.sdk.core.annotation.ProcessOutInternalApi
 @ProcessOutInternalApi
 interface POLoggerService {
 
+    @ProcessOutInternalApi
     fun log(
         level: POLogLevel,
         message: String,
