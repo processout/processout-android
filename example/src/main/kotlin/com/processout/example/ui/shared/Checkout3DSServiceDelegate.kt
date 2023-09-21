b29b73dc683dfb4588cd900e31737408685183eb
Vitalii Vanziak <vitalii.vanziak@processout.com>
2023-09-22 00:30:07 +0300

    Updated Checkout3DSServiceDelegate

--- example/src/main/kotlin/com/processout/example/ui/shared/Checkout3DSServiceDelegate.kt
+++ example/src/main/kotlin/com/processout/example/ui/shared/Checkout3DSServiceDelegate.kt
@@ -1,12 +1,15 @@
 package com.processout.example.ui.shared
 
 import android.app.Activity
+import android.net.Uri
+import com.checkout.threeds.domain.model.UICustomization
 import com.checkout.threeds.standalone.models.ConfigParameters
 import com.checkout.threeds.standalone.models.ThreeDS2ServiceConfiguration
 import com.processout.sdk.api.model.threeds.PO3DSRedirect
 import com.processout.sdk.checkout.threeds.POCheckout3DSServiceDelegate
 import com.processout.sdk.core.ProcessOutResult
 import com.processout.sdk.ui.threeds.PO3DSRedirectCustomTabLauncher
+import java.util.Locale
 
 class Checkout3DSServiceDelegate(
     private val activity: Activity,
@@ -17,7 +20,12 @@ class Checkout3DSServiceDelegate(
     override fun configuration(parameters: ConfigParameters): ThreeDS2ServiceConfiguration {
         return ThreeDS2ServiceConfiguration(
             context = activity,
-            configParameters = parameters
+            configParameters = parameters,
+            // Optional properties.
+            locale = Locale.UK,
+            uiCustomization = UICustomization(),
+            appUri = Uri.parse("https://my-app-url.com"),
+            challengeTimeout = 300
         )
     }
 
