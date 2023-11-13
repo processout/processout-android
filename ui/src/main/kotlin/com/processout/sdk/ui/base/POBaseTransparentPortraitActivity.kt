3f5edc0941e32bb9cf000afae9fc73dabfb403bd
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2023-11-13 13:38:36 +0200

    POBaseTransparentPortraitActivity

---
+++ ui/src/main/kotlin/com/processout/sdk/ui/base/POBaseTransparentPortraitActivity.kt
@@ -0,0 +1,19 @@
+package com.processout.sdk.ui.base
+
+import android.content.pm.ActivityInfo
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class POBaseTransparentPortraitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This is a workaround for crash on Android 8.0 (API level 26).
+        // https://issuetracker.google.com/issues/68454482
+        // https://stackoverflow.com/questions/48072438/java-lang-illegalstateexception-only-fullscreen-opaque-activities-can-request-o
+        requestedOrientation = if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O)
+            ActivityInfo.SCREEN_ORIENTATION_BEHIND
+        else ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+}
--- ui/src/main/kotlin/com/processout/sdk/ui/card/update/POCardUpdateActivity.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/update/POCardUpdateActivity.kt
@@ -1,11 +1,11 @@
 package com.processout.sdk.ui.card.update
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.processout.sdk.ui.base.POBaseTransparentPortraitActivity
 import com.processout.sdk.ui.core.theme.ProcessOutTheme
 
-class POCardUpdateActivity : ComponentActivity() {
+class POCardUpdateActivity : POBaseTransparentPortraitActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
