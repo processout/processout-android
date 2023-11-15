5b4938de777bf613206d6123fca5bbeddd7da5aa
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2023-11-15 18:19:18 +0200

    ProcessOutActivityResult helper extensions

--- sdk/src/main/kotlin/com/processout/sdk/core/ProcessOutActivityResult.kt
+++ sdk/src/main/kotlin/com/processout/sdk/core/ProcessOutActivityResult.kt
@@ -1,8 +1,8 @@
 package com.processout.sdk.core
 
 import android.os.Parcelable
-import com.processout.sdk.core.ProcessOutResult.Failure
-import com.processout.sdk.core.ProcessOutResult.Success
+import com.processout.sdk.core.ProcessOutActivityResult.Failure
+import com.processout.sdk.core.ProcessOutActivityResult.Success
 import kotlinx.parcelize.Parcelize
 
 sealed class ProcessOutActivityResult<out T : Parcelable> : Parcelable {
@@ -18,8 +18,55 @@ sealed class ProcessOutActivityResult<out T : Parcelable> : Parcelable {
 
 fun <T : Parcelable> ProcessOutResult<T>.toActivityResult(): ProcessOutActivityResult<T> =
     when (this) {
-        is Success -> ProcessOutActivityResult.Success(value)
-        is Failure -> ProcessOutActivityResult.Failure(code, message)
+        is ProcessOutResult.Success -> Success(value)
+        is ProcessOutResult.Failure -> Failure(code, message)
+    }
+
+/**
+ * Performs the given action on the encapsulated value if this instance represents [Success].
+ * Returns the original [ProcessOutActivityResult] unchanged.
+ */
+inline fun <T : Parcelable> ProcessOutActivityResult<T>.onSuccess(
+    action: (value: T) -> Unit
+): ProcessOutActivityResult<T> {
+    if (this is Success) {
+        action(value)
+    }
+    return this
+}
+
+/**
+ * Performs the given action on the encapsulated failure if this instance represents [Failure].
+ * Returns the original [ProcessOutActivityResult] unchanged.
+ */
+inline fun <T : Parcelable> ProcessOutActivityResult<T>.onFailure(
+    action: (failure: Failure) -> Unit
+): ProcessOutActivityResult<T> {
+    if (this is Failure) {
+        action(this)
+    }
+    return this
+}
+
+/**
+ * Returns the result of [onSuccess] for the encapsulated value if this instance represents [Success]
+ * or the result of [onFailure] for the encapsulated failure if it is [Failure].
+ */
+inline fun <T : Parcelable, R> ProcessOutActivityResult<T>.fold(
+    onSuccess: (value: T) -> R,
+    onFailure: (failure: Failure) -> R
+): R = when (this) {
+    is Success -> onSuccess(value)
+    is Failure -> onFailure(this)
+}
+
+/**
+ * Returns the encapsulated value if this instance represents [Success] or _null_ if it is [Failure].
+ */
+fun <T : Parcelable> ProcessOutActivityResult<T>.getOrNull(): T? =
+    when (this) {
+        is Success -> value
+        is Failure -> null
     }
 
 @Parcelize
