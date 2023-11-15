8d02fc3ac44ed22291620a4b32b87b4e3b24f9ae
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2023-11-15 18:02:26 +0200

    KDoc for ProcessOutResult extensions

--- sdk/src/main/kotlin/com/processout/sdk/core/ProcessOutResult.kt
+++ sdk/src/main/kotlin/com/processout/sdk/core/ProcessOutResult.kt
@@ -23,6 +23,10 @@ sealed class ProcessOutResult<out T : Any> {
     ) : ProcessOutResult<Nothing>()
 }
 
+/**
+ * Performs the given action on the encapsulated value if this instance represents [Success].
+ * Returns the original [ProcessOutResult] unchanged.
+ */
 inline fun <T : Any> ProcessOutResult<T>.onSuccess(
     action: (value: T) -> Unit
 ): ProcessOutResult<T> {
@@ -32,6 +36,10 @@ inline fun <T : Any> ProcessOutResult<T>.onSuccess(
     return this
 }
 
+/**
+ * Performs the given action on the encapsulated failure if this instance represents [Failure].
+ * Returns the original [ProcessOutResult] unchanged.
+ */
 inline fun <T : Any> ProcessOutResult<T>.onFailure(
     action: (failure: Failure) -> Unit
 ): ProcessOutResult<T> {
@@ -41,6 +49,10 @@ inline fun <T : Any> ProcessOutResult<T>.onFailure(
     return this
 }
 
+/**
+ * Returns the result of [onSuccess] for the encapsulated value if this instance represents [Success]
+ * or the result of [onFailure] for the encapsulated failure if it is [Failure].
+ */
 inline fun <T : Any, R> ProcessOutResult<T>.fold(
     onSuccess: (value: T) -> R,
     onFailure: (failure: Failure) -> R
@@ -49,12 +61,19 @@ inline fun <T : Any, R> ProcessOutResult<T>.fold(
     is Failure -> onFailure(this)
 }
 
+/**
+ * Returns the encapsulated value if this instance represents [Success] or _null_ if it is [Failure].
+ */
 fun <T : Any> ProcessOutResult<T>.getOrNull(): T? =
     when (this) {
         is Success -> value
         is Failure -> null
     }
 
+/**
+ * Returns the encapsulated result of the given transform function applied to the encapsulated value
+ * if this instance represents [Success] or the unmodified copy if it is [Failure].
+ */
 inline fun <T : Any, R : Any> ProcessOutResult<T>.map(
     transform: (T) -> R
 ): ProcessOutResult<R> = when (this) {
@@ -62,6 +81,9 @@ inline fun <T : Any, R : Any> ProcessOutResult<T>.map(
     is Failure -> this.copy()
 }
 
+/**
+ * Returns the unmodified copy of the result.
+ */
 fun <T : Any> ProcessOutResult<T>.copy(): ProcessOutResult<T> =
     when (this) {
         is Success -> this.copy()
