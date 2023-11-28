535622e32a15449ead06b28664e23b2b0e6ee8ab
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2023-11-28 14:41:28 +0200

    CardNumberFormatter

---
+++ ui/src/main/kotlin/com/processout/sdk/ui/shared/formatter/CardNumberFormatter.kt
@@ -0,0 +1,81 @@
+package com.processout.sdk.ui.shared.formatter
+
+import com.processout.sdk.ui.core.formatter.POFormatter
+
+internal class CardNumberFormatter : POFormatter {
+
+    private companion object {
+        const val maxLength = 19 // Maximum PAN length based on ISO/IEC 7812
+        const val placeholderChar = '#'
+        const val defaultPattern = "#### #### #### #### ###"
+    }
+
+    private data class CardNumberFormat(
+        val leading: List<IntRange>,
+        val patterns: List<String>
+    )
+
+    // Reference: https://baymard.com/blog/credit-card-field-auto-format-spaces
+    private val formats = listOf(
+        CardNumberFormat(
+            leading = listOf(34..34, 37..37),
+            patterns = listOf("#### ###### #####")
+        ),
+        CardNumberFormat(
+            leading = listOf(62..62),
+            patterns = listOf("#### #### #### ####", "###### #############")
+        ),
+        CardNumberFormat(
+            leading = listOf(500000..509999, 560000..589999, 600000..699999),
+            patterns = listOf("#### #### #####", "#### ###### #####", "#### #### #### ####", "#### #### #### #### ###")
+        ),
+        CardNumberFormat(
+            leading = listOf(300..305, 309..309, 36..36, 38..39),
+            patterns = listOf("#### ###### ####")
+        ),
+        CardNumberFormat(
+            leading = listOf(1..1),
+            patterns = listOf("#### ##### ######")
+        )
+    )
+
+    override fun format(string: String): String {
+        val cardNumber = string.filter { it.isDigit() }.take(maxLength)
+        formats.forEach { format ->
+            format(cardNumber = cardNumber, format = format)
+                ?.let { return it }
+        }
+        return format(cardNumber = cardNumber, pattern = defaultPattern) ?: string
+    }
+
+    private fun format(cardNumber: String, format: CardNumberFormat): String? {
+        format.leading.forEach { leading ->
+            // First and last values of the range are expected to be of the same length.
+            val leadingLength = leading.first.toString().length
+            if (leading.contains(cardNumber.take(leadingLength).toInt())) {
+                format.patterns.forEach { pattern ->
+                    format(cardNumber = cardNumber, pattern = pattern)
+                        ?.let { return it }
+                }
+            }
+        }
+        return null
+    }
+
+    private fun format(cardNumber: String, pattern: String): String? =
+        buildString {
+            var cardNumberIndex = 0
+            for (index in pattern.indices) {
+                if (cardNumberIndex > cardNumber.lastIndex)
+                    break
+                if (pattern[index] == placeholderChar) {
+                    append(cardNumber[cardNumberIndex])
+                    cardNumberIndex += 1
+                } else {
+                    append(pattern[index])
+                }
+            }
+            if (cardNumberIndex != cardNumber.length)
+                return null
+        }
+}
