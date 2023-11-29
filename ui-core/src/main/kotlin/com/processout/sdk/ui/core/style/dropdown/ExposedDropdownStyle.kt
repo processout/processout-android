235c2513760c389bda6e457b1d6656c76975a90f
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2023-11-29 16:19:37 +0200

    Delete unused ExposedDropdownStyle

--- ui-core/src/main/kotlin/com/processout/sdk/ui/core/style/dropdown/ExposedDropdownStyle.kt
+++
@@ -1,9 +0,0 @@
-package com.processout.sdk.ui.core.style.dropdown
-
-import com.processout.sdk.ui.core.style.input.POInputFieldStyle
-import com.processout.sdk.ui.core.style.input.POInputStyle
-
-internal data class ExposedDropdownStyle(
-    val input: POInputStyle? = null,
-    val dropdownMenu: POInputFieldStyle? = null
-)
