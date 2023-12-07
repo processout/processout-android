69e3d0c7bf936d056098fbb4ffea055f64f62000
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2023-12-07 14:37:49 +0200

    Code reformat

--- sdk/src/production/kotlin/com/processout/sdk/api/network/ApiConstants.kt
+++ sdk/src/production/kotlin/com/processout/sdk/api/network/ApiConstants.kt
@@ -1,7 +1,6 @@
 package com.processout.sdk.api.network
 
 internal object ApiConstants {
-
     const val BASE_URL = "https://api.processout.com"
     const val CHECKOUT_URL = "https://checkout.processout.com"
     const val CHECKOUT_RETURN_URL = "$CHECKOUT_URL/helpers/mobile-processout-webview-landing"
