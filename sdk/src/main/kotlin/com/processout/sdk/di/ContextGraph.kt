8707ef7666eab19bd2253fdd2b59c0b0f27a2a17
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2023-12-13 18:07:22 +0200

    Fix screen size calc for device data

--- sdk/src/main/kotlin/com/processout/sdk/di/ContextGraph.kt
+++ sdk/src/main/kotlin/com/processout/sdk/di/ContextGraph.kt
@@ -1,6 +1,10 @@
 package com.processout.sdk.di
 
 import android.app.Application
+import android.os.Build
+import android.util.Size
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import com.processout.sdk.api.model.request.DeviceData
 import com.processout.sdk.core.locale.currentAppLocale
 import java.util.Calendar
@@ -14,19 +18,33 @@ internal class DefaultContextGraph(
     override val application: Application
 ) : ContextGraph {
 
+    private val screenSize: Size = application.screenSize()
+
     override val deviceData: DeviceData
         get() = provideDeviceData()
 
     private fun provideDeviceData(): DeviceData {
-        val displayMetrics = application.resources.displayMetrics
         val timeZoneOffset = Calendar.getInstance().let {
             -(it.get(Calendar.ZONE_OFFSET) + it.get(Calendar.DST_OFFSET)) / (1000 * 60)
         }
         return DeviceData(
             appLanguage = application.currentAppLocale().toLanguageTag(),
-            appScreenWidth = displayMetrics.widthPixels,
-            appScreenHeight = displayMetrics.heightPixels,
+            appScreenWidth = screenSize.width,
+            appScreenHeight = screenSize.height,
             appTimeZoneOffset = timeZoneOffset
         )
+    }
+
+    private fun Application.screenSize(): Size {
+        val windowManager = ContextCompat.getSystemService(this, WindowManager::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && windowManager != null) {
+            with(windowManager.currentWindowMetrics) {
+                return Size(bounds.width(), bounds.height())
+            }
+        } else {
+            with(resources.displayMetrics) {
+                return Size(widthPixels, heightPixels)
+            }
+        }
     }
 }
