ddb33554c1a3a939e5a12f6881e06a737f2aca35
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-01-24 17:09:40 +0200

    Added failure to POCardUpdateShouldContinueResponse

--- sdk/src/main/kotlin/com/processout/sdk/api/model/response/POCardUpdateShouldContinueResponse.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/POCardUpdateShouldContinueResponse.kt
@@ -1,25 +1,28 @@
 package com.processout.sdk.api.model.response
 
 import com.processout.sdk.api.model.request.POCardUpdateShouldContinueRequest
+import com.processout.sdk.core.ProcessOutResult
 import java.util.UUID
 
 /**
  * Defines the response to decide whether the flow should continue or complete after the failure.
- * This response can only be created from [POCardUpdateShouldContinueRequest.toResponse] to use the same UUID.
+ * This response can only be created from [POCardUpdateShouldContinueRequest.toResponse].
  *
  * @param[uuid] Unique identifier of response that must be equal to UUID of request.
- * @param[shouldContinue] Boolean that indicates whether the flow should continue or complete after the failure.
+ * @param[failure] Failure that occurred. Must be provided from [POCardUpdateShouldContinueRequest].
+ * @param[shouldContinue] Boolean that indicates whether the flow should continue or complete after the [failure].
  */
 data class POCardUpdateShouldContinueResponse internal constructor(
     val uuid: UUID,
+    val failure: ProcessOutResult.Failure,
     val shouldContinue: Boolean
 )
 
 /**
- * Creates response from request to use the same UUID.
+ * Creates [POCardUpdateShouldContinueResponse] from [POCardUpdateShouldContinueRequest].
  *
  * @param[shouldContinue] Boolean that indicates whether the flow should continue or complete after the failure.
  */
 fun POCardUpdateShouldContinueRequest.toResponse(
     shouldContinue: Boolean
-) = POCardUpdateShouldContinueResponse(uuid, shouldContinue)
+) = POCardUpdateShouldContinueResponse(uuid, failure, shouldContinue)
