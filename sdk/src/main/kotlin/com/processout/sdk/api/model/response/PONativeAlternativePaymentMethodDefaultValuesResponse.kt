13eb7f31a38b0ae53e8d40319769a69803ae1c85
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-01-24 14:46:45 +0200

    KDoc

--- sdk/src/main/kotlin/com/processout/sdk/api/model/response/PONativeAlternativePaymentMethodDefaultValuesResponse.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/PONativeAlternativePaymentMethodDefaultValuesResponse.kt
@@ -7,7 +7,7 @@ import java.util.UUID
  * Defines the response with default values for native alternative payment method parameters.
  * This response can only be created from [PONativeAlternativePaymentMethodDefaultValuesRequest.toResponse] to use the same UUID.
  *
- * @param[uuid] Unique identifier of response that must be equal to request UUID.
+ * @param[uuid] Unique identifier of response that must be equal to UUID of request.
  * @param[defaultValues] Map where key is [PONativeAlternativePaymentMethodParameter.key] and value is a default value for this parameter.
  */
 data class PONativeAlternativePaymentMethodDefaultValuesResponse internal constructor(
