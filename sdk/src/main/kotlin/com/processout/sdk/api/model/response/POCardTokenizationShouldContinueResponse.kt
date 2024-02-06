42ed361df50e1dec2ead4509c01a0bc2030a3064
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-02-06 17:31:18 +0200

    POCardTokenizationShouldContinueRequest & POCardTokenizationShouldContinueResponse

---
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/POCardTokenizationShouldContinueResponse.kt
@@ -0,0 +1,31 @@
+package com.processout.sdk.api.model.response
+
+import com.processout.sdk.api.model.request.POCardTokenizationShouldContinueRequest
+import com.processout.sdk.core.ProcessOutResult
+import com.processout.sdk.core.annotation.ProcessOutInternalApi
+import java.util.UUID
+
+/**
+ * Defines the response to decide whether the flow should continue or complete after the failure.
+ * This response can only be created from [POCardTokenizationShouldContinueRequest.toResponse].
+ *
+ * @param[uuid] Unique identifier of response that must be equal to UUID of request.
+ * @param[failure] Failure that occurred. Must be provided from [POCardTokenizationShouldContinueRequest].
+ * @param[shouldContinue] Boolean that indicates whether the flow should continue or complete after the [failure].
+ */
+data class POCardTokenizationShouldContinueResponse internal constructor(
+    val uuid: UUID,
+    val failure: ProcessOutResult.Failure,
+    val shouldContinue: Boolean
+)
+
+/**
+ * Creates [POCardTokenizationShouldContinueResponse] from [POCardTokenizationShouldContinueRequest].
+ *
+ * @param[shouldContinue] Boolean that indicates whether the flow should continue or complete after the failure.
+ */
+/** @suppress */
+@ProcessOutInternalApi
+fun POCardTokenizationShouldContinueRequest.toResponse(
+    shouldContinue: Boolean
+) = POCardTokenizationShouldContinueResponse(uuid, failure, shouldContinue)
