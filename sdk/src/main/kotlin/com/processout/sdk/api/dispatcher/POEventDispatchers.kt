cbccf1c16dc95cabc7a7a898bd6986cf9aa86827
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-02-09 15:39:35 +0200

    cardTokenization accessor via dispatchers

--- sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/POEventDispatchers.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/POEventDispatchers.kt
@@ -1,6 +1,8 @@
 package com.processout.sdk.api.dispatcher
 
+import com.processout.sdk.api.dispatcher.card.tokenization.POCardTokenizationEventDispatcher
 import com.processout.sdk.api.dispatcher.card.update.POCardUpdateEventDispatcher
+import com.processout.sdk.core.annotation.ProcessOutInternalApi
 
 /**
  * Dispatchers that allows to handle events during various payment flows.
@@ -9,6 +11,11 @@ interface POEventDispatchers {
 
     /** Dispatcher that allows to handle events during card updates. */
     val cardUpdate: POCardUpdateEventDispatcher
+
+    /** Dispatcher that allows to handle events during card tokenization. */
+    /** @suppress */
+    @ProcessOutInternalApi
+    val cardTokenization: POCardTokenizationEventDispatcher
 
     /** Dispatcher that allows to handle events during native alternative payments. */
     val nativeAlternativePaymentMethod: PONativeAlternativePaymentMethodEventDispatcher
