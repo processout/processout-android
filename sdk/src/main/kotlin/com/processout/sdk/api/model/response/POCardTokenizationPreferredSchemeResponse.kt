6b4da959afb349039ede74002e9d875d6b41c9f7
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-02-09 15:18:49 +0200

    KDocs

--- sdk/src/main/kotlin/com/processout/sdk/api/model/response/POCardTokenizationPreferredSchemeResponse.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/POCardTokenizationPreferredSchemeResponse.kt
@@ -4,12 +4,27 @@ import com.processout.sdk.api.model.request.POCardTokenizationPreferredSchemeReq
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
 import java.util.UUID
 
+/**
+ * Defines the response with preferred scheme that will be used for card tokenization by default.
+ * This response can only be created from [POCardTokenizationPreferredSchemeRequest.toResponse] function.
+ *
+ * @param[uuid] Unique identifier of response that must be equal to UUID of request.
+ * @param[issuerInformation] Holds information about card issuing institution that issued the card to the card holder.
+ * @param[preferredScheme] Preferred scheme that will be used by default for card tokenization.
+ */
+/** @suppress */
+@ProcessOutInternalApi
 data class POCardTokenizationPreferredSchemeResponse internal constructor(
     val uuid: UUID,
     val issuerInformation: POCardIssuerInformation,
     val preferredScheme: String?
 )
 
+/**
+ * Creates [POCardTokenizationPreferredSchemeResponse] from [POCardTokenizationPreferredSchemeRequest].
+ *
+ * @param[preferredScheme] Preferred scheme that will be used by default for card tokenization.
+ */
 /** @suppress */
 @ProcessOutInternalApi
 fun POCardTokenizationPreferredSchemeRequest.toResponse(
--- sdk/src/main/kotlin/com/processout/sdk/api/model/response/POCardTokenizationShouldContinueResponse.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/POCardTokenizationShouldContinueResponse.kt
@@ -7,12 +7,14 @@ import java.util.UUID
 
 /**
  * Defines the response to decide whether the flow should continue or complete after the failure.
- * This response can only be created from [POCardTokenizationShouldContinueRequest.toResponse].
+ * This response can only be created from [POCardTokenizationShouldContinueRequest.toResponse] function.
  *
  * @param[uuid] Unique identifier of response that must be equal to UUID of request.
  * @param[failure] Failure that occurred. Must be provided from [POCardTokenizationShouldContinueRequest].
  * @param[shouldContinue] Boolean that indicates whether the flow should continue or complete after the [failure].
  */
+/** @suppress */
+@ProcessOutInternalApi
 data class POCardTokenizationShouldContinueResponse internal constructor(
     val uuid: UUID,
     val failure: ProcessOutResult.Failure,
