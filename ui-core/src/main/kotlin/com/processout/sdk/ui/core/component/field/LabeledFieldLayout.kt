1ef2bc0a189df9b8b357d92fe1a1cb287a42b8e7
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-04-16 17:10:35 +0300

    LabeledFieldLayout

---
+++ ui-core/src/main/kotlin/com/processout/sdk/ui/core/component/field/LabeledFieldLayout.kt
@@ -0,0 +1,38 @@
+package com.processout.sdk.ui.core.component.field
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.processout.sdk.ui.core.component.POExpandableText
+import com.processout.sdk.ui.core.component.POText
+import com.processout.sdk.ui.core.theme.ProcessOutTheme
+
+@Composable
+internal fun LabeledFieldLayout(
+    title: String,
+    description: String?,
+    modifier: Modifier = Modifier,
+    style: POFieldLabels.Style = POFieldLabels.default,
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        POText(
+            text = title,
+            modifier = Modifier.padding(bottom = ProcessOutTheme.spacing.small),
+            color = style.title.color,
+            style = style.title.textStyle
+        )
+        content()
+        POExpandableText(
+            text = description,
+            style = style.description,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = ProcessOutTheme.spacing.small)
+        )
+    }
+}
