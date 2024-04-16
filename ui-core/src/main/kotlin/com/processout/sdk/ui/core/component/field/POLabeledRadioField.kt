9d612cc0e7487630942b02b57277268cff910e2b
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-04-16 18:51:50 +0300

    POLabeledRadioField

---
+++ ui-core/src/main/kotlin/com/processout/sdk/ui/core/component/field/POLabeledRadioField.kt
@@ -0,0 +1,42 @@
+package com.processout.sdk.ui.core.component.field
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import com.processout.sdk.ui.core.annotation.ProcessOutInternalApi
+import com.processout.sdk.ui.core.state.POAvailableValue
+import com.processout.sdk.ui.core.state.POImmutableList
+
+/** @suppress */
+@ProcessOutInternalApi
+@Composable
+fun POLabeledRadioField(
+    value: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    availableValues: POImmutableList<POAvailableValue>,
+    title: String,
+    description: String?,
+    modifier: Modifier = Modifier,
+    radioGroupStyle: PORadioGroup.Style = PORadioGroup.default,
+    labelsStyle: POFieldLabels.Style = POFieldLabels.default,
+    isError: Boolean = false,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    LabeledFieldLayout(
+        title = title,
+        description = description,
+        style = labelsStyle
+    ) {
+        PORadioGroup(
+            value = value.text,
+            onValueChange = { onValueChange(TextFieldValue(text = it)) },
+            availableValues = availableValues,
+            modifier = modifier,
+            style = radioGroupStyle,
+            isError = isError,
+            interactionSource = interactionSource
+        )
+    }
+}
