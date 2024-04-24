e5465d0eab1b641ddc831cfb90f3032b11a48d7b
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-04-24 18:19:34 +0300

    Catch Google Pay API exception

--- ui/src/main/kotlin/com/processout/sdk/ui/googlepay/GooglePayService.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/googlepay/GooglePayService.kt
@@ -1,12 +1,14 @@
 package com.processout.sdk.ui.googlepay
 
 import android.app.Application
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.wallet.IsReadyToPayRequest
 import com.google.android.gms.wallet.PaymentData
 import com.google.android.gms.wallet.PaymentDataRequest
 import com.google.android.gms.wallet.Wallet
 import com.google.android.gms.wallet.Wallet.WalletOptions
+import com.processout.sdk.core.logger.POLogger
 import kotlinx.coroutines.tasks.await
 import org.json.JSONObject
 
@@ -17,10 +19,14 @@ internal class GooglePayService(
 
     private val client = Wallet.getPaymentsClient(application, walletOptions)
 
-    suspend fun isReadyToPay(isReadyToPayRequestJson: JSONObject): Boolean {
-        val request = IsReadyToPayRequest.fromJson(isReadyToPayRequestJson.toString())
-        return client.isReadyToPay(request).await()
-    }
+    suspend fun isReadyToPay(isReadyToPayRequestJson: JSONObject): Boolean =
+        try {
+            val request = IsReadyToPayRequest.fromJson(isReadyToPayRequestJson.toString())
+            client.isReadyToPay(request).await()
+        } catch (e: ApiException) {
+            POLogger.warn("Google Pay API exception when checking readiness: %s", e)
+            false
+        }
 
     fun loadPaymentData(paymentDataRequestJson: JSONObject): Task<PaymentData> {
         val request = PaymentDataRequest.fromJson(paymentDataRequestJson.toString())
