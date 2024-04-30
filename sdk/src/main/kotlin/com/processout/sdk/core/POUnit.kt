943e6a550e1ca654325c8cb3c78aad1195077b44
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-04-30 16:26:26 +0300

    POUnit

---
+++ sdk/src/main/kotlin/com/processout/sdk/core/POUnit.kt
@@ -0,0 +1,11 @@
+package com.processout.sdk.core
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Parcelable unit object.
+ * Represents the absence of meaningful value.
+ */
+@Parcelize
+object POUnit : Parcelable
