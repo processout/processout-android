3387bf62900298d4d4958ceb7114eb88cd31ec89
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-05-10 18:16:42 +0300

    NativeAlternativePaymentViewModelState

--- ui/src/main/kotlin/com/processout/sdk/ui/napm/NativeAlternativePaymentViewModelState.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/napm/NativeAlternativePaymentViewModelState.kt
@@ -1,17 +1,22 @@
 package com.processout.sdk.ui.napm
 
+import androidx.compose.runtime.Immutable
 import com.processout.sdk.ui.core.state.POActionState
 
+@Immutable
 internal sealed interface NativeAlternativePaymentViewModelState {
     data object Loading : NativeAlternativePaymentViewModelState
 
+    @Immutable
     data class UserInput(
         val title: String,
         val primaryAction: POActionState,
         val secondaryAction: POActionState?
     ) : NativeAlternativePaymentViewModelState
 
+    @Immutable
     data class Capture(
-        val secondaryAction: POActionState?
+        val secondaryAction: POActionState?,
+        val isSuccess: Boolean
     ) : NativeAlternativePaymentViewModelState
 }
