9720a51005df495e9bad82e07f44b783584686ca
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-05-15 20:24:11 +0300

    KDoc

--- sdk/src/main/kotlin/com/processout/sdk/ui/nativeapm/PONativeAlternativePaymentMethodConfiguration.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/nativeapm/PONativeAlternativePaymentMethodConfiguration.kt
@@ -89,6 +89,15 @@ data class PONativeAlternativePaymentMethodConfiguration(
         ) : SecondaryAction()
     }
 
+    /**
+     * Specifies action confirmation behaviour and values.
+     *
+     * @param[enabled] Enables action confirmation.
+     * @param[title] Custom title. Pass _null_ to use default text.
+     * @param[message] Custom message. Pass _null_ to use default text.
+     * @param[positiveActionText] Custom positive action text. Pass _null_ to use default text.
+     * @param[negativeActionText] Custom negative action text. Pass _null_ to use default text.
+     */
     @Parcelize
     data class ActionConfirmation(
         val enabled: Boolean = false,
