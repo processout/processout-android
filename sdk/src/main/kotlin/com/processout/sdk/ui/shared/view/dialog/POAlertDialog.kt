aea3bfa27c18eb8e3aac5219a0c67a7899412f41
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-05-15 15:28:56 +0300

    Apply style

--- sdk/src/main/kotlin/com/processout/sdk/ui/shared/view/dialog/POAlertDialog.kt
+++ sdk/src/main/kotlin/com/processout/sdk/ui/shared/view/dialog/POAlertDialog.kt
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.ContextThemeWrapper
 import com.processout.sdk.R
 import com.processout.sdk.databinding.PoAlertDialogBinding
+import com.processout.sdk.ui.nativeapm.applyStyle
+import com.processout.sdk.ui.shared.style.dialog.PODialogStyle
 
 internal class POAlertDialog(
-    context: Context
+    context: Context,
+    style: PODialogStyle? = null
 ) : AlertDialog(
     context,
     R.style.ThemeOverlay_ProcessOut_AlertDialog
@@ -20,5 +23,14 @@ internal class POAlertDialog(
 
     init {
         setView(binding.root)
+        style?.apply()
+    }
+
+    private fun PODialogStyle.apply() {
+        binding.root.setBackgroundColor(backgroundColor)
+        binding.poTitle.applyStyle(title)
+        binding.poMessage.applyStyle(message)
+        binding.poPositiveButton.applyStyle(positiveButton)
+        binding.poNegativeButton.applyStyle(negativeButton)
     }
 }
