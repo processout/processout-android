e546c7b534ac56511a5af51a92128df0a26628ec
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-05-20 21:33:09 +0300

    Layout and colors

--- ui-core/src/main/kotlin/com/processout/sdk/ui/core/component/PODialog.kt
+++ ui-core/src/main/kotlin/com/processout/sdk/ui/core/component/PODialog.kt
@@ -1,10 +1,23 @@
 package com.processout.sdk.ui.core.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.processout.sdk.ui.core.annotation.ProcessOutInternalApi
+import com.processout.sdk.ui.core.component.PODialog.ScrimColor
+import com.processout.sdk.ui.core.component.PODialog.cardColors
 import com.processout.sdk.ui.core.style.PODialogStyle
 import com.processout.sdk.ui.core.theme.ProcessOutTheme
 
@@ -12,9 +25,39 @@ import com.processout.sdk.ui.core.theme.ProcessOutTheme
 @ProcessOutInternalApi
 @Composable
 fun PODialog(
+    title: String,
+    message: String?,
+    confirmActionText: String,
+    dismissActionText: String?,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
     style: PODialog.Style = PODialog.default
 ) {
+    Dialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = false,
+            usePlatformDefaultWidth = false
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = ScrimColor),
+            contentAlignment = Alignment.Center
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(ProcessOutTheme.spacing.extraLarge),
+                shape = ProcessOutTheme.shapes.roundedCornersLarge,
+                colors = cardColors(style.backgroundColor)
+            ) {
 
+            }
+        }
+    }
 }
 
 /** @suppress */
@@ -49,4 +92,13 @@ object PODialog {
             backgroundColor = colorResource(id = backgroundColorResId)
         )
     }
+
+    internal val ScrimColor = Color.Black.copy(alpha = 0.32f)
+
+    internal fun cardColors(backgroundColor: Color) = CardColors(
+        containerColor = backgroundColor,
+        contentColor = Color.Unspecified,
+        disabledContainerColor = Color.Unspecified,
+        disabledContentColor = Color.Unspecified
+    )
 }
