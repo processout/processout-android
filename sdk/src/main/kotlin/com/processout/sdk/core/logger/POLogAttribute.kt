a6177cedbae792a8f6a101b09ebca15de6775689
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-06-07 18:22:56 +0300

    POLogAttribute

---
+++ sdk/src/main/kotlin/com/processout/sdk/core/logger/POLogAttribute.kt
@@ -0,0 +1,13 @@
+package com.processout.sdk.core.logger
+
+import com.processout.sdk.core.annotation.ProcessOutInternalApi
+
+/** @suppress */
+@ProcessOutInternalApi
+object POLogAttribute {
+    const val CARD_ID = "CardId"
+    const val INVOICE_ID = "InvoiceId"
+    const val CUSTOMER_ID = "CustomerId"
+    const val CUSTOMER_TOKEN_ID = "CustomerTokenId"
+    const val GATEWAY_CONFIGURATION_ID = "GatewayConfigurationId"
+}
