41a64f557f23db0781575a544099e559f4da376d
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-06-10 13:05:26 +0300

    Extend ProcessOutConfiguration

--- sdk/src/main/kotlin/com/processout/sdk/api/ProcessOutConfiguration.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/ProcessOutConfiguration.kt
@@ -10,12 +10,28 @@ import com.processout.sdk.core.annotation.ProcessOutInternalApi
  * @param[application] Instance of the [Application].
  * @param[projectId] Project ID.
  * @param[debug] Enables debug mode. Default value is _false_. __Note:__ debug logs may contain sensitive data.
+ * @param[enableTelemetry] Enables sending telemetry data to ProcessOut. Default value is _false_.
+ * @param[applicationInformation] Application information that helps ProcessOut to troubleshoot potential issues.
  */
 data class ProcessOutConfiguration(
     val application: Application,
     val projectId: String,
-    val debug: Boolean = false
+    val debug: Boolean = false,
+    val enableTelemetry: Boolean = false,
+    val applicationInformation: ApplicationInformation? = null
 ) {
+
+    /**
+     * Application information that helps ProcessOut to troubleshoot potential issues.
+     *
+     * @param[name] Application name.
+     * @param[version] Application version.
+     */
+    data class ApplicationInformation(
+        val name: String? = null,
+        val version: String? = null
+    )
+
     /**
      * __Warning:__ Intended to be used only for testing purposes.
      * Storing private key inside application is extremely dangerous and is highly discouraged.
