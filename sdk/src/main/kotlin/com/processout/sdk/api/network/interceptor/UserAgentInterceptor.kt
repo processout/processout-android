7cdc55757c83f29705c97d3fde984c83de1d4ca9
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-06-10 17:21:06 +0300

    Product-Version header

--- sdk/src/main/kotlin/com/processout/sdk/api/network/interceptor/UserAgentInterceptor.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/network/interceptor/UserAgentInterceptor.kt
@@ -25,6 +25,7 @@ internal class UserAgentInterceptor(
             .header("User-Agent", userAgentComponents.joinToString(separator = "/"))
             .header("Accept-Language", contextGraph.configuration.application.currentSdkLocale().toLanguageTag())
             .header("Session-Id", contextGraph.configuration.sessionId)
+            .header("Product-Version", sdkVersion)
             .build()
         return chain.proceed(userAgentRequest)
     }
