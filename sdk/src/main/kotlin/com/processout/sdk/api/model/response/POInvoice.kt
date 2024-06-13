95512f29e9d89c5e1bd6a90de340ca3e033c4e23
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-06-13 17:06:17 +0300

    Extended POInvoice

--- sdk/src/main/kotlin/com/processout/sdk/api/model/response/POInvoice.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/POInvoice.kt
@@ -1,15 +1,23 @@
 package com.processout.sdk.api.model.response
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 /**
  * Invoice details.
  *
  * @param[id] Invoice identifier.
+ * @param[amount] Invoice amount.
+ * @param[currency] Invoice currency.
+ * @param[returnUrl] Return URL or deep link for web based operations.
  */
 @JsonClass(generateAdapter = true)
 data class POInvoice(
-    val id: String
+    val id: String,
+    val amount: String,
+    val currency: String,
+    @Json(name = "return_url")
+    val returnUrl: String?
 )
 
 @JsonClass(generateAdapter = true)
