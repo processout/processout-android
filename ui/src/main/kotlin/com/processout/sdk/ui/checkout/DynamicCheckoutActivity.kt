1b9057eee4db80f000508570cc6f2ccca2c883df
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-06-17 19:59:55 +0300

    Transparent portrait activity

--- ui/src/main/kotlin/com/processout/sdk/ui/checkout/DynamicCheckoutActivity.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/checkout/DynamicCheckoutActivity.kt
@@ -1,12 +1,12 @@
 package com.processout.sdk.ui.checkout
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.processout.sdk.ui.base.BaseTransparentPortraitActivity
 import com.processout.sdk.ui.core.component.POText
 import com.processout.sdk.ui.core.theme.ProcessOutTheme
 
-internal class DynamicCheckoutActivity : ComponentActivity() {
+internal class DynamicCheckoutActivity : BaseTransparentPortraitActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
