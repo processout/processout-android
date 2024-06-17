a243a8342392ce975ee4f36280cce371d2d98e63
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-06-17 19:52:59 +0300

    Example: DC feature navigation

--- example/src/main/kotlin/com/processout/example/ui/screen/features/FeaturesFragment.kt
+++ example/src/main/kotlin/com/processout/example/ui/screen/features/FeaturesFragment.kt
@@ -54,6 +54,11 @@ class FeaturesFragment : BaseFragment<FragmentFeaturesBinding>(
         super.onViewCreated(view, savedInstanceState)
         val navController = findNavController()
         with(binding) {
+            dynamicCheckoutButton.setOnClickListener {
+                navController.navigate(
+                    FeaturesFragmentDirections.actionFeaturesFragmentToDynamicCheckoutFragment()
+                )
+            }
             nativeApmButton.setOnClickListener {
                 navController.navigate(
                     FeaturesFragmentDirections.actionFeaturesFragmentToAlternativePaymentMethodsFragment(
