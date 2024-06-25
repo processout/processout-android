2a5ab4205b3fc9f8f6ae0cee1d434001e21c7c86
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-06-25 19:59:39 +0300

    DynamicCheckoutActivity animation

--- ui/src/main/kotlin/com/processout/sdk/ui/checkout/DynamicCheckoutActivity.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/checkout/DynamicCheckoutActivity.kt
@@ -1,7 +1,13 @@
 package com.processout.sdk.ui.checkout
 
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.processout.sdk.ui.R
 import com.processout.sdk.ui.base.BaseTransparentPortraitActivity
 import com.processout.sdk.ui.core.component.POText
 import com.processout.sdk.ui.core.theme.ProcessOutTheme
@@ -10,10 +16,24 @@ internal class DynamicCheckoutActivity : BaseTransparentPortraitActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.BLACK)
+        )
         setContent {
             ProcessOutTheme {
                 POText(text = "DynamicCheckoutActivity")
             }
+        }
+    }
+
+    override fun finish() {
+        super.finish()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            overrideActivityTransition(Activity.OVERRIDE_TRANSITION_CLOSE, 0, R.anim.po_slide_out_vertical)
+        } else {
+            @Suppress("DEPRECATION")
+            overridePendingTransition(0, R.anim.po_slide_out_vertical)
         }
     }
 }
