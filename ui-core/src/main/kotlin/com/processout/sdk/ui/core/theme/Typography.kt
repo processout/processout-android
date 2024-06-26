3669fb50bb9c1e21b988823b473496215b8b157c
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-06-26 13:22:15 +0300

    Typography

--- ui-core/src/main/kotlin/com/processout/sdk/ui/core/theme/Typography.kt
+++ ui-core/src/main/kotlin/com/processout/sdk/ui/core/theme/Typography.kt
@@ -1,20 +1,12 @@
 package com.processout.sdk.ui.core.theme
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.processout.sdk.ui.core.R
 import com.processout.sdk.ui.core.annotation.ProcessOutInternalApi
@@ -30,6 +22,48 @@ private val WorkSans = FontFamily(
 @ProcessOutInternalApi
 @Immutable
 data class POTypography(
+    val title: TextStyle = TextStyle(
+        fontFamily = WorkSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 24.sp
+    ),
+    val subheading: TextStyle = TextStyle(
+        fontFamily = WorkSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = 24.sp
+    ),
+    val body1: TextStyle = TextStyle(
+        fontFamily = WorkSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    val body2: TextStyle = TextStyle(
+        fontFamily = WorkSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    ),
+    val button: TextStyle = TextStyle(
+        fontFamily = WorkSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    ),
+    val label1: TextStyle = TextStyle(
+        fontFamily = WorkSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    ),
+    val label2: TextStyle = TextStyle(
+        fontFamily = WorkSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    ),
     val fixed: Fixed = Fixed(),
     val medium: Medium = Medium()
 ) {
@@ -58,12 +92,6 @@ data class POTypography(
             fontWeight = FontWeight.Medium,
             fontSize = 14.sp,
             lineHeight = 14.sp
-        ),
-        val caption: TextStyle = TextStyle(
-            fontFamily = WorkSans,
-            fontWeight = FontWeight.Normal,
-            fontSize = 12.sp,
-            lineHeight = 16.sp
         )
     )
 
@@ -74,12 +102,6 @@ data class POTypography(
             fontWeight = FontWeight.Medium,
             fontSize = 20.sp,
             lineHeight = 28.sp
-        ),
-        val subtitle: TextStyle = TextStyle(
-            fontFamily = WorkSans,
-            fontWeight = FontWeight.Medium,
-            fontSize = 18.sp,
-            lineHeight = 24.sp
         ),
         val body: TextStyle = TextStyle(
             fontFamily = WorkSans,
@@ -112,42 +134,3 @@ private fun POTextType.Weight.toFontWeight(): FontWeight =
         EXTRA_BOLD -> FontWeight.ExtraBold
         BLACK -> FontWeight.Black
     }
-
-@Composable
-@Preview(showBackground = true)
-private fun POTypographyPreview() {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "Typography Medium Title",
-            style = LocalPOTypography.current.medium.title
-        )
-        Text(
-            text = "Typography Medium Subtitle",
-            style = LocalPOTypography.current.medium.subtitle
-        )
-        Text(
-            text = "Typography Fixed Body",
-            style = LocalPOTypography.current.fixed.body
-        )
-        Text(
-            text = "Typography Fixed Label",
-            style = LocalPOTypography.current.fixed.label
-        )
-        Text(
-            text = "Typography Fixed Label Heading",
-            style = LocalPOTypography.current.fixed.labelHeading
-        )
-        Text(
-            text = "Typography Fixed Button",
-            style = LocalPOTypography.current.fixed.button
-        )
-        Text(
-            text = "Typography Fixed Caption",
-            style = LocalPOTypography.current.fixed.caption
-        )
-    }
-}
