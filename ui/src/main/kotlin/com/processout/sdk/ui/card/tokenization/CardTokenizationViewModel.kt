09bf497a8171415aa2ad86f13d47d53ae1eba332
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-07-08 15:44:52 +0300

    Separate create() method in nAPM and CardTokenization VM Factories

--- ui/src/main/kotlin/com/processout/sdk/ui/card/tokenization/CardTokenizationViewModel.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/tokenization/CardTokenizationViewModel.kt
@@ -41,19 +41,20 @@ internal class CardTokenizationViewModel private constructor(
         private val configuration: POCardTokenizationConfiguration
     ) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
-        override fun <T : ViewModel> create(modelClass: Class<T>): T =
-            CardTokenizationViewModel(
+        override fun <T : ViewModel> create(modelClass: Class<T>): T = create() as T
+
+        fun create() = CardTokenizationViewModel(
+            app = app,
+            configuration = configuration,
+            interactor = CardTokenizationInteractor(
                 app = app,
                 configuration = configuration,
-                interactor = CardTokenizationInteractor(
-                    app = app,
-                    configuration = configuration,
-                    cardsRepository = ProcessOut.instance.cards,
-                    cardSchemeProvider = CardSchemeProvider(),
-                    addressSpecificationProvider = AddressSpecificationProvider(app),
-                    eventDispatcher = PODefaultCardTokenizationEventDispatcher
-                )
-            ) as T
+                cardsRepository = ProcessOut.instance.cards,
+                cardSchemeProvider = CardSchemeProvider(),
+                addressSpecificationProvider = AddressSpecificationProvider(app),
+                eventDispatcher = PODefaultCardTokenizationEventDispatcher
+            )
+        )
     }
 
     private data class KeyboardAction(
