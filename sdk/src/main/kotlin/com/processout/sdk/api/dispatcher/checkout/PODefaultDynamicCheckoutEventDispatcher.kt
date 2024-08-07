a09696f44f13807b646ff19ac8844a0987e022ce
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-08-07 20:00:34 +0300

    Define dispatcher events

--- sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/checkout/PODefaultDynamicCheckoutEventDispatcher.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/checkout/PODefaultDynamicCheckoutEventDispatcher.kt
@@ -1,9 +1,28 @@
 package com.processout.sdk.api.dispatcher.checkout
 
+import com.processout.sdk.api.model.event.POCardTokenizationEvent
+import com.processout.sdk.api.model.event.PONativeAlternativePaymentMethodEvent
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
 /** @suppress */
 @ProcessOutInternalApi
 object PODefaultDynamicCheckoutEventDispatcher : PODynamicCheckoutEventDispatcher {
 
+    private val _cardTokenizationEvents = MutableSharedFlow<POCardTokenizationEvent>()
+    override val cardTokenizationEvents = _cardTokenizationEvents.asSharedFlow()
+
+    private val _nativeAlternativePaymentEvents = MutableSharedFlow<PONativeAlternativePaymentMethodEvent>()
+    override val nativeAlternativePaymentEvents = _nativeAlternativePaymentEvents.asSharedFlow()
+
+    // Events
+
+    suspend fun send(event: POCardTokenizationEvent) {
+        _cardTokenizationEvents.emit(event)
+    }
+
+    suspend fun send(event: PONativeAlternativePaymentMethodEvent) {
+        _nativeAlternativePaymentEvents.emit(event)
+    }
 }
--- sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/checkout/PODynamicCheckoutEventDispatcher.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/checkout/PODynamicCheckoutEventDispatcher.kt
@@ -1,9 +1,15 @@
 package com.processout.sdk.api.dispatcher.checkout
 
+import com.processout.sdk.api.model.event.POCardTokenizationEvent
+import com.processout.sdk.api.model.event.PONativeAlternativePaymentMethodEvent
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
+import kotlinx.coroutines.flow.SharedFlow
 
 /** @suppress */
 @ProcessOutInternalApi
 interface PODynamicCheckoutEventDispatcher {
 
+    val cardTokenizationEvents: SharedFlow<POCardTokenizationEvent>
+
+    val nativeAlternativePaymentEvents: SharedFlow<PONativeAlternativePaymentMethodEvent>
 }
