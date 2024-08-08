d8d020719a925406d81fa50a5ec089e00e4cbc99
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-08-08 18:43:53 +0300

    Extend dispatcher with invoice request/response

--- sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/checkout/PODefaultDynamicCheckoutEventDispatcher.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/checkout/PODefaultDynamicCheckoutEventDispatcher.kt
@@ -2,6 +2,8 @@ package com.processout.sdk.api.dispatcher.checkout
 
 import com.processout.sdk.api.model.event.POCardTokenizationEvent
 import com.processout.sdk.api.model.event.PONativeAlternativePaymentMethodEvent
+import com.processout.sdk.api.model.request.PODynamicCheckoutInvoiceRequest
+import com.processout.sdk.api.model.response.PODynamicCheckoutInvoiceResponse
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -16,6 +18,12 @@ object PODefaultDynamicCheckoutEventDispatcher : PODynamicCheckoutEventDispatche
     private val _nativeAlternativePaymentEvents = MutableSharedFlow<PONativeAlternativePaymentMethodEvent>()
     override val nativeAlternativePaymentEvents = _nativeAlternativePaymentEvents.asSharedFlow()
 
+    private val _invoiceRequest = MutableSharedFlow<PODynamicCheckoutInvoiceRequest>()
+    override val invoiceRequest = _invoiceRequest.asSharedFlow()
+
+    private val _invoiceResponse = MutableSharedFlow<PODynamicCheckoutInvoiceResponse>()
+    val invoiceResponse = _invoiceResponse.asSharedFlow()
+
     // Events
 
     suspend fun send(event: POCardTokenizationEvent) {
@@ -25,4 +33,16 @@ object PODefaultDynamicCheckoutEventDispatcher : PODynamicCheckoutEventDispatche
     suspend fun send(event: PONativeAlternativePaymentMethodEvent) {
         _nativeAlternativePaymentEvents.emit(event)
     }
+
+    // Invoice
+
+    suspend fun send(request: PODynamicCheckoutInvoiceRequest) {
+        _invoiceRequest.emit(request)
+    }
+
+    override suspend fun replaceInvoice(response: PODynamicCheckoutInvoiceResponse) {
+        _invoiceResponse.emit(response)
+    }
+
+    fun subscribedForInvoiceRequest() = _invoiceRequest.subscriptionCount.value > 0
 }
--- sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/checkout/PODynamicCheckoutEventDispatcher.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/checkout/PODynamicCheckoutEventDispatcher.kt
@@ -2,6 +2,8 @@ package com.processout.sdk.api.dispatcher.checkout
 
 import com.processout.sdk.api.model.event.POCardTokenizationEvent
 import com.processout.sdk.api.model.event.PONativeAlternativePaymentMethodEvent
+import com.processout.sdk.api.model.request.PODynamicCheckoutInvoiceRequest
+import com.processout.sdk.api.model.response.PODynamicCheckoutInvoiceResponse
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
 import kotlinx.coroutines.flow.SharedFlow
 
@@ -12,4 +14,8 @@ interface PODynamicCheckoutEventDispatcher {
     val cardTokenizationEvents: SharedFlow<POCardTokenizationEvent>
 
     val nativeAlternativePaymentEvents: SharedFlow<PONativeAlternativePaymentMethodEvent>
+
+    val invoiceRequest: SharedFlow<PODynamicCheckoutInvoiceRequest>
+
+    suspend fun replaceInvoice(response: PODynamicCheckoutInvoiceResponse)
 }
