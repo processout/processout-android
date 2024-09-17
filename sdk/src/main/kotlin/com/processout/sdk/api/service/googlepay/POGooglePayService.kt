50fda61e0f8d31392780b01152a297e827a12730
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-09-17 13:25:47 +0300

    POGooglePayService moved from 'ui' module to 'sdk'

---
+++ sdk/src/main/kotlin/com/processout/sdk/api/service/googlepay/POGooglePayService.kt
@@ -0,0 +1,14 @@
+package com.processout.sdk.api.service.googlepay
+
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.wallet.PaymentData
+import com.processout.sdk.core.annotation.ProcessOutInternalApi
+import org.json.JSONObject
+
+@ProcessOutInternalApi
+interface POGooglePayService {
+
+    suspend fun isReadyToPay(isReadyToPayRequestJson: JSONObject): Boolean
+
+    fun loadPaymentData(paymentDataRequestJson: JSONObject): Task<PaymentData>
+}
