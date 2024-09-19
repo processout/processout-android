13401e36807c0e6df5521952246b5ca6c368d3ca
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-09-19 14:38:58 +0300

    Extracted POGooglePayConfiguration

--- sdk/src/main/kotlin/com/processout/sdk/api/service/googlepay/POGooglePayConfiguration.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/service/googlepay/POGooglePayConfiguration.kt
@@ -1,21 +1,16 @@
 package com.processout.sdk.api.service.googlepay
 
-import com.processout.sdk.api.service.googlepay.POGooglePayConfiguration.PaymentDataParameters.TransactionInfo
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
-import org.json.JSONArray
-import org.json.JSONObject
 
 @ProcessOutInternalApi
-object POGooglePayConfiguration {
+data class POGooglePayConfiguration(
+    val gateway: String,
+    val gatewayMerchantId: String,
+    val card: CardConfiguration,
+    val paymentData: PaymentDataConfiguration
+) {
 
-    data class Parameters(
-        val gateway: String,
-        val gatewayMerchantId: String,
-        val cardParameters: CardParameters,
-        val paymentDataParameters: PaymentDataParameters
-    )
-
-    data class CardParameters(
+    data class CardConfiguration(
         val allowedAuthMethods: Set<String>,
         val allowedCardNetworks: Set<String>,
         val allowPrepaidCards: Boolean,
@@ -35,7 +30,7 @@ object POGooglePayConfiguration {
         }
     }
 
-    data class PaymentDataParameters(
+    data class PaymentDataConfiguration(
         val transactionInfo: TransactionInfo,
         val merchantName: String?,
         val emailRequired: Boolean,
@@ -66,102 +61,4 @@ object POGooglePayConfiguration {
             val phoneNumberRequired: Boolean
         )
     }
-
-    fun isReadyToPayRequest(parameters: CardParameters): JSONObject =
-        baseRequest.put("allowedPaymentMethods", allowedPaymentMethods(parameters))
-
-    fun allowedPaymentMethods(parameters: CardParameters): JSONArray =
-        JSONArray().put(cardPaymentMethod(parameters))
-
-    fun paymentDataRequest(parameters: Parameters): JSONObject =
-        with(parameters) {
-            val shippingAddressParameters = paymentDataParameters.shippingAddressParameters
-            val request = baseRequest
-                .put(
-                    "allowedPaymentMethods", JSONArray().put(
-                        cardPaymentMethod(cardParameters)
-                            .put(
-                                "tokenizationSpecification", tokenizationSpecification(
-                                    gateway = gateway,
-                                    gatewayMerchantId = gatewayMerchantId
-                                )
-                            )
-                    )
-                )
-                .put("transactionInfo", transactionInfo(paymentDataParameters.transactionInfo))
-                .put("emailRequired", paymentDataParameters.emailRequired)
-                .put("shippingAddressRequired", paymentDataParameters.shippingAddressRequired)
-            if (!paymentDataParameters.merchantName.isNullOrBlank()) {
-                request.put(
-                    "merchantInfo", JSONObject()
-                        .put("merchantName", paymentDataParameters.merchantName)
-                )
-            }
-            if (paymentDataParameters.shippingAddressRequired && shippingAddressParameters != null) {
-                request.put(
-                    "shippingAddressParameters", JSONObject()
-                        .put("allowedCountryCodes", JSONArray(shippingAddressParameters.allowedCountryCodes))
-                        .put("phoneNumberRequired", shippingAddressParameters.phoneNumberRequired)
-                )
-            }
-            request
-        }
-
-    private val baseRequest = JSONObject()
-        .put("apiVersion", 2)
-        .put("apiVersionMinor", 0)
-
-    private fun cardPaymentMethod(parameters: CardParameters): JSONObject =
-        with(parameters) {
-            val request = JSONObject()
-                .put("type", "CARD")
-                .put(
-                    "parameters", JSONObject()
-                        .put("allowedAuthMethods", JSONArray(allowedAuthMethods))
-                        .put("allowedCardNetworks", JSONArray(allowedCardNetworks))
-                        .put("allowPrepaidCards", allowPrepaidCards)
-                        .put("allowCreditCards", allowCreditCards)
-                        .put("assuranceDetailsRequired", assuranceDetailsRequired)
-                        .put("billingAddressRequired", billingAddressRequired)
-                )
-            if (billingAddressRequired && billingAddressParameters != null) {
-                request.put(
-                    "billingAddressParameters", JSONObject()
-                        .put("format", billingAddressParameters.format.toString())
-                        .put("phoneNumberRequired", billingAddressParameters.phoneNumberRequired)
-                )
-            }
-            request
-        }
-
-    private fun tokenizationSpecification(
-        gateway: String,
-        gatewayMerchantId: String
-    ) = JSONObject()
-        .put("type", "PAYMENT_GATEWAY")
-        .put(
-            "parameters", JSONObject(
-                mapOf(
-                    "gateway" to gateway,
-                    "gatewayMerchantId" to gatewayMerchantId
-                )
-            )
-        )
-
-    private fun transactionInfo(parameters: TransactionInfo): JSONObject =
-        with(parameters) {
-            val transactionInfo = JSONObject()
-                .put("currencyCode", currencyCode)
-                .put("transactionId", transactionId)
-                .put("totalPrice", totalPrice)
-                .put("totalPriceStatus", totalPriceStatus.toString())
-                .put("checkoutOption", checkoutOption.toString())
-            if (!countryCode.isNullOrBlank()) {
-                transactionInfo.put("countryCode", countryCode)
-            }
-            if (!totalPriceLabel.isNullOrBlank()) {
-                transactionInfo.put("totalPriceLabel", totalPriceLabel)
-            }
-            transactionInfo
-        }
 }
