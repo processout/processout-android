1e37497b86aae2c7c9622d63dbc94cf9ace54d47
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-10-08 17:10:21 +0300

    enableEdgeToEdge() in nAPM (Compose) and Card activities

--- ui/src/main/kotlin/com/processout/sdk/ui/card/update/CardUpdateActivity.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/update/CardUpdateActivity.kt
@@ -1,12 +1,14 @@
 package com.processout.sdk.ui.card.update
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.processout.sdk.ui.base.BaseTransparentPortraitActivity
 
 internal class CardUpdateActivity : BaseTransparentPortraitActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         if (savedInstanceState == null) {
             val bottomSheet = supportFragmentManager.findFragmentByTag(CardUpdateBottomSheet.tag)
             if (bottomSheet == null) {
