d7777ee1933d7eeafbd7a0c0ade3bd53a861f159
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-10-08 16:42:31 +0300

    Adjust screenModeAsState()

--- ui/src/main/kotlin/com/processout/sdk/ui/shared/component/Screen.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/shared/component/Screen.kt
@@ -1,17 +1,13 @@
 package com.processout.sdk.ui.shared.component
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.processout.sdk.ui.shared.component.ScreenMode.Fullscreen
 import com.processout.sdk.ui.shared.component.ScreenMode.Window
-import com.processout.sdk.ui.shared.extension.dpToPx
+import com.processout.sdk.ui.shared.extension.screenSize
 
 internal sealed interface ScreenMode {
     data class Window(val height: Int, val availableHeight: Int) : ScreenMode
@@ -20,16 +16,14 @@ internal sealed interface ScreenMode {
 
 @Composable
 internal fun screenModeAsState(viewHeight: Int): State<ScreenMode> {
-    val isImeVisible = isImeVisibleAsState().value
-    val screenHeight = LocalConfiguration.current.screenHeightDp.dp.dpToPx()
-    val availableHeight = if (isImeVisible) screenHeight - imeHeight() + navigationBarHeight() else screenHeight
-    val screenMode = remember {
-        mutableStateOf<ScreenMode>(Window(height = viewHeight, availableHeight = availableHeight))
+    val isImeVisible by isImeVisibleAsState()
+    val totalViewHeight = if (isImeVisible) viewHeight + imeHeight() else viewHeight
+    val screenHeight = LocalContext.current.screenSize().height + navigationBarHeight()
+    val screenMode = remember(totalViewHeight, screenHeight) {
+        mutableStateOf<ScreenMode>(Window(height = totalViewHeight, availableHeight = screenHeight))
     }
-    if (availableHeight < viewHeight) {
+    if (screenHeight < totalViewHeight) {
         screenMode.value = Fullscreen(screenHeight)
-    } else {
-        screenMode.value = Window(height = viewHeight, availableHeight = availableHeight)
     }
     return screenMode
 }
