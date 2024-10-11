cb2a25feac4d916e00957d45dd7df6b7f15a1b05
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-10-11 12:50:01 +0300

    enableEdgeToEdge in example app

--- example/src/main/kotlin/com/processout/example/ui/screen/MainActivity.kt
+++ example/src/main/kotlin/com/processout/example/ui/screen/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.processout.example.ui.screen
 
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT))
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
