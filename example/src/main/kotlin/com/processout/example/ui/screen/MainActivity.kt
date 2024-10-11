7c90daab6beac2deab5c386c49018545433ec2b6
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-10-11 13:18:28 +0300

    adjustInsets in example app

--- example/src/main/kotlin/com/processout/example/ui/screen/MainActivity.kt
+++ example/src/main/kotlin/com/processout/example/ui/screen/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.processout.example.ui.screen
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -15,12 +17,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT))
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                ContextCompat.getColor(this, com.processout.sdk.R.color.po_action_primary_default)
+            )
+        )
+        adjustInsets()
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun adjustInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(
+            findViewById(android.R.id.content)
+        ) { content, insets ->
+            val padding = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+                        or WindowInsetsCompat.Type.displayCutout()
+                        or WindowInsetsCompat.Type.ime()
+            )
+            content.setPaddingRelative(
+                padding.left,
+                padding.top,
+                padding.right,
+                padding.bottom
+            )
+            insets
+        }
     }
 }
