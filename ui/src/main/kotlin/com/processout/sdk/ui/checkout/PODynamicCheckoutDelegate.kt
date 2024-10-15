ada678beb1692808eca84332e903de84e1e30bfe
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-10-15 17:53:26 +0300

    invoiceAuthorizationRequest() in delegate

--- ui/src/main/kotlin/com/processout/sdk/ui/checkout/PODynamicCheckoutDelegate.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/checkout/PODynamicCheckoutDelegate.kt
@@ -3,7 +3,9 @@ package com.processout.sdk.ui.checkout
 import com.processout.sdk.api.model.event.POCardTokenizationEvent
 import com.processout.sdk.api.model.event.PONativeAlternativePaymentMethodEvent
 import com.processout.sdk.api.model.request.PODynamicCheckoutInvoiceInvalidationReason
+import com.processout.sdk.api.model.request.POInvoiceAuthorizationRequest
 import com.processout.sdk.api.model.request.POInvoiceRequest
+import com.processout.sdk.api.model.response.PODynamicCheckoutPaymentMethod
 import com.processout.sdk.api.model.response.POInvoice
 import com.processout.sdk.ui.core.annotation.ProcessOutInternalApi
 
@@ -22,4 +24,13 @@ interface PODynamicCheckoutDelegate {
         currentInvoice: POInvoice,
         invalidationReason: PODynamicCheckoutInvoiceInvalidationReason
     ): POInvoiceRequest? = null
+
+    /**
+     * Called before invoice authorization.
+     * Allows to alter request parameters but please make sure that _invoiceId_ and _source_ are unmodified.
+     */
+    suspend fun invoiceAuthorizationRequest(
+        request: POInvoiceAuthorizationRequest,
+        paymentMethod: PODynamicCheckoutPaymentMethod
+    ): POInvoiceAuthorizationRequest = request
 }
