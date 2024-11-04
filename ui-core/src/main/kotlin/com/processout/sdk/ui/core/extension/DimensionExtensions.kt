cf96bcb6da3328edfb796c7409237c69ff0087ca
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-11-04 18:43:17 +0200

    Cleanup

--- ui-core/src/main/kotlin/com/processout/sdk/ui/core/extension/DimensionExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.processout.sdk.ui.core.extension
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
-
-@Composable
-internal fun Dp.dpToPx() = with(LocalDensity.current) { this@dpToPx.roundToPx() }
