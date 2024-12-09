384b330413785080c0596d8b38c714e0adc0fdd8
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2024-12-09 19:07:46 +0200

    Removed @ProcessOutInternalApi from barcode type and nAPM config field

--- ui/src/main/kotlin/com/processout/sdk/ui/shared/configuration/POBarcodeConfiguration.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/shared/configuration/POBarcodeConfiguration.kt
@@ -1,7 +1,6 @@
 package com.processout.sdk.ui.shared.configuration
 
 import android.os.Parcelable
-import com.processout.sdk.core.annotation.ProcessOutInternalApi
 import kotlinx.parcelize.Parcelize
 
 /**
@@ -10,7 +9,6 @@ import kotlinx.parcelize.Parcelize
  * @param[saveActionText] Text on the button that saves barcode.
  * @param[saveErrorConfirmation] Requests user confirmation (e.g. dialog) when saving barcode has failed. Use _null_ to disable.
  */
-@ProcessOutInternalApi
 @Parcelize
 data class POBarcodeConfiguration(
     val saveActionText: String? = null,
