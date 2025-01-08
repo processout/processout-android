b6ab222ae6dd22bf2f2159aa610ce5341260e761
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-01-08 12:53:44 +0200

    Update POBarcodeConfiguration constructor

--- ui/src/main/kotlin/com/processout/sdk/ui/shared/configuration/POBarcodeConfiguration.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/shared/configuration/POBarcodeConfiguration.kt
@@ -7,34 +7,27 @@ import kotlinx.parcelize.Parcelize
 /**
  * Specifies barcode configuration.
  *
- * @param[saveActionText] Text on the save button.
  * @param[saveButton] Save button configuration.
  * @param[saveErrorConfirmation] Requests user confirmation (e.g. dialog) when barcode saving has failed. Use _null_ to disable.
  */
 @Parcelize
-data class POBarcodeConfiguration internal constructor(
-    @Deprecated(message = "Use 'saveButton.text' instead.")
-    val saveActionText: String? = null,
+data class POBarcodeConfiguration(
     val saveButton: Button = Button(),
     val saveErrorConfirmation: POActionConfirmationConfiguration? = POActionConfirmationConfiguration()
 ) : Parcelable {
 
+    /**
+     * Specifies barcode configuration.
+     *
+     * @param[saveActionText] Save button text.
+     * @param[saveErrorConfirmation] Requests user confirmation (e.g. dialog) when barcode saving has failed. Use _null_ to disable.
+     */
     @Deprecated(message = "Use alternative constructor.")
     constructor(
         saveActionText: String? = null,
         saveErrorConfirmation: POActionConfirmationConfiguration? = POActionConfirmationConfiguration()
     ) : this(
-        saveActionText = saveActionText,
         saveButton = Button(text = saveActionText),
-        saveErrorConfirmation = saveErrorConfirmation
-    )
-
-    constructor(
-        saveButton: Button = Button(),
-        saveErrorConfirmation: POActionConfirmationConfiguration? = POActionConfirmationConfiguration()
-    ) : this(
-        saveActionText = saveButton.text,
-        saveButton = saveButton,
         saveErrorConfirmation = saveErrorConfirmation
     )
 
