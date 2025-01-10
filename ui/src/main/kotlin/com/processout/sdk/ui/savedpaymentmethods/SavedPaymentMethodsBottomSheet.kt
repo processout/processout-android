56fb6160c36d79b6a507676cc778916df05fc9c4
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-01-10 14:43:15 +0200

    Init VM

--- ui/src/main/kotlin/com/processout/sdk/ui/savedpaymentmethods/SavedPaymentMethodsBottomSheet.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/savedpaymentmethods/SavedPaymentMethodsBottomSheet.kt
@@ -7,12 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.viewModels
+import com.processout.sdk.api.model.request.POInvoiceRequest
+import com.processout.sdk.core.POFailure.Code.Generic
 import com.processout.sdk.core.POUnit
 import com.processout.sdk.core.ProcessOutResult
 import com.processout.sdk.ui.base.BaseBottomSheetDialogFragment
 import com.processout.sdk.ui.core.component.POText
 import com.processout.sdk.ui.core.theme.ProcessOutTheme
 import com.processout.sdk.ui.savedpaymentmethods.SavedPaymentMethodsActivityContract.Companion.EXTRA_CONFIGURATION
+import com.processout.sdk.ui.savedpaymentmethods.SavedPaymentMethodsEvent.Dismiss
 import kotlin.math.roundToInt
 
 internal class SavedPaymentMethodsBottomSheet : BaseBottomSheetDialogFragment<POUnit>() {
@@ -26,10 +30,31 @@ internal class SavedPaymentMethodsBottomSheet : BaseBottomSheetDialogFragment<PO
 
     private var configuration: POSavedPaymentMethodsConfiguration? = null
 
+    private val viewModel: SavedPaymentMethodsViewModel by viewModels {
+        SavedPaymentMethodsViewModel.Factory(
+            app = requireActivity().application,
+            configuration = configuration ?: POSavedPaymentMethodsConfiguration(
+                invoiceRequest = POInvoiceRequest(invoiceId = String())
+            )
+        )
+    }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         @Suppress("DEPRECATION")
         configuration = arguments?.getParcelable(EXTRA_CONFIGURATION)
+        configuration?.run {
+            if (invoiceRequest.invoiceId.isBlank()) {
+                viewModel.onEvent(
+                    Dismiss(
+                        ProcessOutResult.Failure(
+                            code = Generic(),
+                            message = "Invalid configuration."
+                        )
+                    )
+                )
+            }
+        }
     }
 
     override fun onCreateView(
