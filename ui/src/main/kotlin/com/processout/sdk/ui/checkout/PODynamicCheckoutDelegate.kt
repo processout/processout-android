ae6de4e9562923d4d7e0108c59a4669df3c75703
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-02-13 18:58:21 +0200

    kdoc

--- ui/src/main/kotlin/com/processout/sdk/ui/checkout/PODynamicCheckoutDelegate.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/checkout/PODynamicCheckoutDelegate.kt
@@ -61,15 +61,27 @@ interface PODynamicCheckoutDelegate {
         paymentMethod: PODynamicCheckoutPaymentMethod
     ): POInvoiceAuthorizationRequest = request
 
+    /**
+     * Allows to choose default preferred card scheme based on issuer information.
+     * Primary card scheme is used by default.
+     */
     suspend fun preferredScheme(
         issuerInformation: POCardIssuerInformation
     ): String? = issuerInformation.scheme
 
+    /**
+     * Allows to prefill default values for the given parameters during native alternative payment.
+     * Return a map where key is a [PONativeAlternativePaymentMethodParameter.key] and value is a custom default value.
+     * It's not mandatory to provide default values for all parameters.
+     */
     suspend fun defaultValues(
         gatewayConfigurationId: String,
         parameters: List<PONativeAlternativePaymentMethodParameter>
     ): Map<String, String> = emptyMap()
 
+    /**
+     * Allows to customize saved payment methods configuration.
+     */
     suspend fun savedPaymentMethods(
         configuration: POSavedPaymentMethodsConfiguration
     ): POSavedPaymentMethodsConfiguration = configuration
