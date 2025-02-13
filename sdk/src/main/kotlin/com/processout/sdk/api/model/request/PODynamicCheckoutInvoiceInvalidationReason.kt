bf53d0a6ab16efa57dbccbdb72bda107144753c2
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-02-13 16:51:18 +0200

    PODynamicCheckoutInvoiceInvalidationReason

--- sdk/src/main/kotlin/com/processout/sdk/api/model/request/PODynamicCheckoutInvoiceInvalidationReason.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/request/PODynamicCheckoutInvoiceInvalidationReason.kt
@@ -3,10 +3,20 @@ package com.processout.sdk.api.model.request
 import com.processout.sdk.core.ProcessOutResult
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
 
+/**
+ * Invoice invalidation reason during dynamic checkout flow.
+ */
 /** @suppress */
 @ProcessOutInternalApi
 sealed class PODynamicCheckoutInvoiceInvalidationReason {
+    /**
+     * Indicates that user has selected different payment method in a state that requires new invoice.
+     */
     data object PaymentMethodChanged : PODynamicCheckoutInvoiceInvalidationReason()
+
+    /**
+     * Indicates that selected method has failed and payment can't be continued with the current invoice.
+     */
     data class Failure(
         val failure: ProcessOutResult.Failure
     ) : PODynamicCheckoutInvoiceInvalidationReason()
