445e414510552bc1f7312cc849ff892691148410
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-02-18 13:59:08 +0200

    POCardScannerConfiguration

---
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/POCardScannerConfiguration.kt
@@ -0,0 +1,41 @@
+package com.processout.sdk.ui.card.scanner
+
+import android.os.Parcelable
+import com.processout.sdk.ui.core.shared.image.PODrawableImage
+import com.processout.sdk.ui.core.style.POButtonStyle
+import com.processout.sdk.ui.core.style.POTextStyle
+import com.processout.sdk.ui.shared.configuration.POActionConfirmationConfiguration
+import com.processout.sdk.ui.shared.configuration.POCancellationConfiguration
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class POCardScannerConfiguration(
+    val title: String? = null,
+    val description: String? = null,
+    val cancelButton: CancelButton? = CancelButton(),
+    val cancellation: POCancellationConfiguration = POCancellationConfiguration(),
+    val style: Style? = null
+) : Parcelable {
+
+    /**
+     * Cancel button configuration.
+     *
+     * @param[text] Button text. Pass _null_ to use default text.
+     * @param[icon] Button icon drawable resource. Pass _null_ to hide.
+     * @param[confirmation] Specifies action confirmation configuration (e.g. dialog).
+     * Use _null_ to disable, this is a default behaviour.
+     */
+    @Parcelize
+    data class CancelButton(
+        val text: String? = null,
+        val icon: PODrawableImage? = null,
+        val confirmation: POActionConfirmationConfiguration? = null
+    ) : Parcelable
+
+    @Parcelize
+    data class Style(
+        val title: POTextStyle? = null,
+        val description: POTextStyle? = null,
+        val cancelButton: POButtonStyle? = null
+    ) : Parcelable
+}
