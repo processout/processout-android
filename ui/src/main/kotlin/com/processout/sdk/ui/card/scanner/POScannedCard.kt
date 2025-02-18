dc23997b1527f6d1345bfb46e4a08189604255d2
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-02-18 13:29:04 +0200

    POScannedCard

---
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/POScannedCard.kt
@@ -0,0 +1,18 @@
+package com.processout.sdk.ui.card.scanner
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class POScannedCard(
+    val number: String,
+    val expiration: Expiration?,
+    val cardholderName: String?
+) : Parcelable {
+
+    @Parcelize
+    data class Expiration(
+        val month: Int,
+        val year: Int
+    ) : Parcelable
+}
