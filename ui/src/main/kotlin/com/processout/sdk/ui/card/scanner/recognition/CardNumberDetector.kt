9fd31fc181b90aaa476cb59572281b4ecf3a6ade
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-03-06 13:57:57 +0200

    CardNumberDetector

--- ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/recognition/CardNumberDetector.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/recognition/CardNumberDetector.kt
@@ -2,8 +2,30 @@ package com.processout.sdk.ui.card.scanner.recognition
 
 internal class CardNumberDetector : CardAttributeDetector<String> {
 
+    private val numberRegex = Regex("(?:\\d\\s*){12,19}")
+
     override fun firstMatch(candidates: List<String>): String? {
-        // TODO
+        candidates.forEach { candidate ->
+            numberRegex.find(candidate)?.let { match ->
+                val number = match.value.filterNot { it.isWhitespace() }
+                if (isLuhnChecksumValid(number)) {
+                    return number
+                }
+            }
+        }
         return null
+    }
+
+    private fun isLuhnChecksumValid(number: String): Boolean {
+        val reversedDigits = number.reversed().map { it.digitToInt() }
+        val checksum = reversedDigits.mapIndexed { index, digit ->
+            if (index % 2 == 1) {
+                val doubled = digit * 2
+                if (doubled > 9) doubled - 9 else doubled
+            } else {
+                digit
+            }
+        }.sum()
+        return checksum % 10 == 0
     }
 }
