644104c45106b45e3dcf7b1356457e2712859536
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-03-06 18:37:38 +0200

    CardExpirationDetector

---
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/recognition/CardExpirationDetector.kt
@@ -0,0 +1,31 @@
+package com.processout.sdk.ui.card.scanner.recognition
+
+import java.util.Calendar
+
+internal class CardExpirationDetector(
+    private val calendar: Calendar = Calendar.getInstance()
+) : CardAttributeDetector<POScannedCard.Expiration> {
+
+    private val expirationRegex = """(0?[1-9]|1[0-2])[/\\.-](\d{4}|\d{2})""".toRegex()
+    private val separators = listOf('/', '\\', '.', '-')
+
+    override fun firstMatch(candidates: List<String>): POScannedCard.Expiration? {
+        candidates.reversed().forEach { candidate ->
+            val filtered = candidate.filter { it.isDigit() || separators.contains(it) }
+            expirationRegex.find(filtered)?.let { match ->
+                val month = match.groupValues[1].toInt()
+                val year = normalized(match.groupValues[2].toInt())
+                return POScannedCard.Expiration(month = month, year = year)
+            }
+        }
+        return null
+    }
+
+    private fun normalized(year: Int): Int {
+        if (year >= 100) {
+            return year
+        }
+        val currentYear = calendar.get(Calendar.YEAR)
+        return (currentYear / 100) * 100 + year
+    }
+}
