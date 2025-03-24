d64c5f0b0800f89aa8bcc537dad50581a0dbcbe9
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-03-24 13:01:48 +0200

    POScannedCard KDoc

--- ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/recognition/POScannedCard.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/recognition/POScannedCard.kt
@@ -3,6 +3,13 @@ package com.processout.sdk.ui.card.scanner.recognition
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Scanned card details.
+ *
+ * @param[number] Formatted card number.
+ * @param[expiration] Card expiration details.
+ * @param[cardholderName] Cardholder name.
+ */
 @Parcelize
 data class POScannedCard(
     val number: String,
@@ -10,6 +17,14 @@ data class POScannedCard(
     val cardholderName: String?
 ) : Parcelable {
 
+    /**
+     * Card expiration details.
+     *
+     * @param[month] Expiration month.
+     * @param[year] Expiration year as a four digits number.
+     * @param[isExpired] Indicates whether the expiration date has past, making the card expired.
+     * @param[formatted] Formatted month and year.
+     */
     @Parcelize
     data class Expiration(
         val month: Int,
