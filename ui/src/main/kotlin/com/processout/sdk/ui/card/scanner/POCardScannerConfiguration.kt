7da66978533b4af0133f7842db1e3c8d6168cad3
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-03-24 13:59:31 +0200

    POCardScannerConfiguration KDoc

--- ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/POCardScannerConfiguration.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/scanner/POCardScannerConfiguration.kt
@@ -11,6 +11,17 @@ import com.processout.sdk.ui.shared.configuration.POActionConfirmationConfigurat
 import com.processout.sdk.ui.shared.configuration.POCancellationConfiguration
 import kotlinx.parcelize.Parcelize
 
+/**
+ * Specifies card scanner configuration.
+ *
+ * @param[title] Custom title.
+ * @param[description] Custom description.
+ * @param[cancelButton] Cancel button configuration. Pass _null_ to hide.
+ * @param[cancellation] Specifies cancellation behaviour.
+ * @param[shouldScanExpiredCard] Specifies whether the card scanner allows to scan expired cards.
+ * Default value is _false_.
+ * @param[style] Custom style.
+ */
 @Parcelize
 data class POCardScannerConfiguration(
     val title: String? = null,
@@ -36,6 +47,18 @@ data class POCardScannerConfiguration(
         val confirmation: POActionConfirmationConfiguration? = null
     ) : Parcelable
 
+    /**
+     * Specifies card scanner style.
+     *
+     * @param[title] Title style.
+     * @param[description] Description style.
+     * @param[cameraPreview] Camera preview style.
+     * @param[card] Scanned card details style.
+     * @param[torchToggle] Torch toggle button style.
+     * @param[cancelButton] Cancel button style.
+     * @param[dialog] Dialog style.
+     * @param[backgroundColorResId] Color resource ID for background.
+     */
     @Parcelize
     data class Style(
         val title: POTextStyle? = null,
@@ -49,6 +72,12 @@ data class POCardScannerConfiguration(
         val backgroundColorResId: Int? = null
     ) : Parcelable
 
+    /**
+     * Specifies the style of a camera preview.
+     *
+     * @param[border] Border style.
+     * @param[overlayColorResId] Color resource ID for camera preview overlay.
+     */
     @Parcelize
     data class CameraPreviewStyle(
         val border: POBorderStyle,
@@ -56,6 +85,14 @@ data class POCardScannerConfiguration(
         val overlayColorResId: Int
     ) : Parcelable
 
+    /**
+     * Specifies the style of a scanned card details view.
+     *
+     * @param[number] Card number style.
+     * @param[expiration] Card expiration style.
+     * @param[cardholderName] Cardholder name style.
+     * @param[border] Border style.
+     */
     @Parcelize
     data class CardStyle(
         val number: POTextStyle,
