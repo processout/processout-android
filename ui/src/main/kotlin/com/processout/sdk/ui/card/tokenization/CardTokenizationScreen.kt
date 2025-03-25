a3502ffe2a38f9d5f626067e9f53b3d26bcd3fd8
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-03-25 17:33:32 +0200

    clear focus

--- ui/src/main/kotlin/com/processout/sdk/ui/card/tokenization/CardTokenizationScreen.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/card/tokenization/CardTokenizationScreen.kt
@@ -94,10 +94,14 @@ internal fun CardTokenizationScreen(
                     onContentHeightChanged(contentHeight)
                 }
             ) {
+                var clearFocus by remember { mutableStateOf(false) }
                 state.scanAction?.let { action ->
                     POButton(
                         state = action,
-                        onClick = { onEvent(Action(id = it)) },
+                        onClick = {
+                            clearFocus = true
+                            onEvent(Action(id = it))
+                        },
                         modifier = Modifier
                             .fillMaxWidth()
                             .requiredHeightIn(min = dimensions.buttonIconSizeSmall)
@@ -111,6 +115,10 @@ internal fun CardTokenizationScreen(
                     onEvent = onEvent,
                     style = style
                 )
+                if (clearFocus || state.focusedFieldId == null) {
+                    LocalFocusManager.current.clearFocus(force = true)
+                    clearFocus = false
+                }
             }
         }
     }
@@ -122,9 +130,6 @@ private fun Sections(
     onEvent: (CardTokenizationEvent) -> Unit,
     style: CardTokenizationScreen.Style
 ) {
-    if (state.focusedFieldId == null) {
-        LocalFocusManager.current.clearFocus(force = true)
-    }
     val lifecycleEvent = rememberLifecycleEvent()
     state.sections.elements.forEachIndexed { index, section ->
         val padding = if (section.id == FUTURE_PAYMENTS) {
