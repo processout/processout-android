acc6e48a364ccb6efa347825e30eaa21bddb6d5f
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-04-23 18:32:12 +0300

    Use POCardScheme in CardSchemeDrawableResProvider

--- ui/src/main/kotlin/com/processout/sdk/ui/shared/provider/CardSchemeDrawableResProvider.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/shared/provider/CardSchemeDrawableResProvider.kt
@@ -1,30 +1,33 @@
 package com.processout.sdk.ui.shared.provider
 
 import androidx.annotation.DrawableRes
+import com.processout.sdk.api.model.response.POCardScheme
+import com.processout.sdk.api.model.response.POCardScheme.*
 import com.processout.sdk.ui.R
+import com.processout.sdk.ui.shared.extension.findBy
 
 @DrawableRes
 internal fun cardSchemeDrawableResId(scheme: String): Int? =
-    when (scheme.lowercase()) {
-        "american express" -> R.drawable.po_scheme_amex
-        "carte bancaire" -> R.drawable.po_scheme_carte_bancaire
-        "dinacard" -> R.drawable.po_scheme_dinacard
-        "diners club" -> R.drawable.po_scheme_diners
-        "diners club carte blanche" -> R.drawable.po_scheme_diners
-        "diners club international" -> R.drawable.po_scheme_diners
-        "diners club united states & canada" -> R.drawable.po_scheme_diners
-        "discover" -> R.drawable.po_scheme_discover
-        "elo" -> R.drawable.po_scheme_elo
-        "giropay" -> R.drawable.po_scheme_giropay
-        "jcb" -> R.drawable.po_scheme_jcb
-        "mada" -> R.drawable.po_scheme_mada
-        "maestro" -> R.drawable.po_scheme_maestro
-        "mastercard" -> R.drawable.po_scheme_mastercard
-        "rupay" -> R.drawable.po_scheme_rupay
-        "sodexo" -> R.drawable.po_scheme_sodexo
-        "china union pay" -> R.drawable.po_scheme_union_pay
-        "verve" -> R.drawable.po_scheme_verve
-        "visa" -> R.drawable.po_scheme_visa
-        "vpay" -> R.drawable.po_scheme_vpay
+    when (POCardScheme::rawValue.findBy(scheme)) {
+        AMEX -> R.drawable.po_scheme_amex
+        CARTE_BANCAIRE -> R.drawable.po_scheme_carte_bancaire
+        DINA_CARD -> R.drawable.po_scheme_dinacard
+        DINERS_CLUB,
+        DINERS_CLUB_CARTE_BLANCHE,
+        DINERS_CLUB_INTERNATIONAL,
+        DINERS_CLUB_UNITED_STATES_AND_CANADA -> R.drawable.po_scheme_diners
+        DISCOVER -> R.drawable.po_scheme_discover
+        ELO -> R.drawable.po_scheme_elo
+        GIROPAY -> R.drawable.po_scheme_giropay
+        JCB -> R.drawable.po_scheme_jcb
+        MADA -> R.drawable.po_scheme_mada
+        MAESTRO -> R.drawable.po_scheme_maestro
+        MASTERCARD -> R.drawable.po_scheme_mastercard
+        RUPAY -> R.drawable.po_scheme_rupay
+        SODEXO -> R.drawable.po_scheme_sodexo
+        UNION_PAY -> R.drawable.po_scheme_union_pay
+        VERVE -> R.drawable.po_scheme_verve
+        VISA -> R.drawable.po_scheme_visa
+        V_PAY -> R.drawable.po_scheme_vpay
         else -> null
     }
