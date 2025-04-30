95b5d3369c262d446dfe38d19a1fd8c4eeaaa5f5
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-04-30 17:31:51 +0300

    DynamicCheckoutAlternativePaymentConfiguration.kt (request/response)

---
+++ ui/src/main/kotlin/com/processout/sdk/ui/checkout/dispatcher/DynamicCheckoutAlternativePaymentConfiguration.kt
@@ -0,0 +1,21 @@
+package com.processout.sdk.ui.checkout.dispatcher
+
+import com.processout.sdk.api.dispatcher.POEventDispatcher
+import com.processout.sdk.api.model.response.PODynamicCheckoutPaymentMethod
+import com.processout.sdk.ui.checkout.PODynamicCheckoutConfiguration
+import java.util.UUID
+
+internal data class DynamicCheckoutAlternativePaymentConfigurationRequest(
+    override val uuid: UUID = UUID.randomUUID(),
+    val paymentMethod: PODynamicCheckoutPaymentMethod.AlternativePayment,
+    val configuration: PODynamicCheckoutConfiguration.AlternativePaymentConfiguration
+) : POEventDispatcher.Request
+
+internal data class DynamicCheckoutAlternativePaymentConfigurationResponse(
+    override val uuid: UUID,
+    val configuration: PODynamicCheckoutConfiguration.AlternativePaymentConfiguration
+) : POEventDispatcher.Response
+
+internal fun DynamicCheckoutAlternativePaymentConfigurationRequest.toResponse(
+    configuration: PODynamicCheckoutConfiguration.AlternativePaymentConfiguration
+) = DynamicCheckoutAlternativePaymentConfigurationResponse(uuid, configuration)
