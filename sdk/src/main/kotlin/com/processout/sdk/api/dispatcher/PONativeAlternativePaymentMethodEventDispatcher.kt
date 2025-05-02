f011b4c6a4b36a92f60ad4d4c1e87a558dd9597e
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-05-02 16:26:13 +0300

    Remove TODO

--- sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/PONativeAlternativePaymentMethodEventDispatcher.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/dispatcher/PONativeAlternativePaymentMethodEventDispatcher.kt
@@ -5,8 +5,6 @@ import com.processout.sdk.api.model.request.PONativeAlternativePaymentMethodDefa
 import com.processout.sdk.api.model.response.PONativeAlternativePaymentMethodDefaultValuesResponse
 import kotlinx.coroutines.flow.SharedFlow
 
-// TODO: Move it to '.dispatcher.napm' package before next major release.
-
 /**
  * Dispatcher that allows to handle events during native alternative payments.
  */
