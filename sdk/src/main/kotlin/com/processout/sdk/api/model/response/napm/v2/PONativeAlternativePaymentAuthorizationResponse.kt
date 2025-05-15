8b16db902e3b45235b819948cafd89e36fad866b
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-05-15 19:12:12 +0300

    Remove NativeAlternativePaymentAuthorizationResponseBody

--- sdk/src/main/kotlin/com/processout/sdk/api/model/response/napm/v2/PONativeAlternativePaymentAuthorizationResponse.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/napm/v2/PONativeAlternativePaymentAuthorizationResponse.kt
@@ -1,13 +1,16 @@
 package com.processout.sdk.api.model.response.napm.v2
 
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
+import com.squareup.moshi.JsonClass
 
 /** @suppress */
 @ProcessOutInternalApi
+@JsonClass(generateAdapter = true)
 data class PONativeAlternativePaymentAuthorizationResponse(
     val state: State
 ) {
 
+    @JsonClass(generateAdapter = false)
     enum class State {
         NEXT_STEP_REQUIRED,
         PENDING_CAPTURE,
--- sdk/src/main/kotlin/com/processout/sdk/api/repository/DefaultInvoicesRepository.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/repository/DefaultInvoicesRepository.kt
@@ -4,13 +4,9 @@ import com.processout.sdk.api.model.request.*
 import com.processout.sdk.api.model.request.napm.v2.NativeAlternativePaymentAuthorizationRequestBody
 import com.processout.sdk.api.model.request.napm.v2.PONativeAlternativePaymentAuthorizationRequest
 import com.processout.sdk.api.model.response.*
-import com.processout.sdk.api.model.response.napm.v2.NativeAlternativePaymentAuthorizationResponseBody
-import com.processout.sdk.api.model.response.napm.v2.NativeAlternativePaymentAuthorizationResponseBody.State.*
-import com.processout.sdk.api.model.response.napm.v2.PONativeAlternativePaymentAuthorizationResponse
 import com.processout.sdk.api.network.HeaderConstants.CLIENT_SECRET
 import com.processout.sdk.api.network.InvoicesApi
 import com.processout.sdk.core.*
-import com.processout.sdk.core.POFailure.Code.Generic
 import com.processout.sdk.di.ContextGraph
 import kotlinx.coroutines.launch
 import retrofit2.Response
@@ -38,7 +34,7 @@ internal class DefaultInvoicesRepository(
             invoiceId = request.invoiceId,
             request = request.toBody()
         )
-    }.map()
+    }
 
     override suspend fun initiatePayment(
         request: PONativeAlternativePaymentMethodRequest
@@ -146,31 +142,6 @@ internal class DefaultInvoicesRepository(
             gatewayConfigurationId = gatewayConfigurationId,
             nativeApm = NativeAPMRequestParameters(parameterValues = parameters)
         )
-
-    private fun ProcessOutResult<NativeAlternativePaymentAuthorizationResponseBody>.map() = fold(
-        onSuccess = { responseBody ->
-            when (responseBody.state) {
-                FAILED -> ProcessOutResult.Failure( // TODO: map values from response body
-                    code = Generic(),
-                    message = ""
-                )
-                else -> ProcessOutResult.Success(
-                    PONativeAlternativePaymentAuthorizationResponse(
-                        state = when (responseBody.state) {
-                            NEXT_STEP_REQUIRED -> PONativeAlternativePaymentAuthorizationResponse.State.NEXT_STEP_REQUIRED
-                            PENDING_CAPTURE -> PONativeAlternativePaymentAuthorizationResponse.State.PENDING_CAPTURE
-                            CAPTURED -> PONativeAlternativePaymentAuthorizationResponse.State.CAPTURED
-                            else -> return ProcessOutResult.Failure(
-                                code = Generic(),
-                                message = "Unsupported payment state: ${responseBody.state}."
-                            )
-                        }
-                    )
-                )
-            }
-        },
-        onFailure = { it }
-    )
 
     private fun ProcessOutResult<Response<InvoiceResponse>>.map() = fold(
         onSuccess = { response ->
