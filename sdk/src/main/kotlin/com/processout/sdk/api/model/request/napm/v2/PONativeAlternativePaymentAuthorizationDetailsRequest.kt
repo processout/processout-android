c89b004f841cc18dde8f78f8759cf2baf21ce364
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-05-28 16:31:05 +0300

    KDoc

--- sdk/src/main/kotlin/com/processout/sdk/api/model/request/napm/v2/PONativeAlternativePaymentAuthorizationDetailsRequest.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/request/napm/v2/PONativeAlternativePaymentAuthorizationDetailsRequest.kt
@@ -3,7 +3,7 @@ package com.processout.sdk.api.model.request.napm.v2
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
 
 /**
- * Request parameters for native alternative payment.
+ * Request parameters for native alternative payment authorization details.
  *
  * @param[invoiceId] Invoice identifier.
  * @param[gatewayConfigurationId] Gateway configuration identifier.
