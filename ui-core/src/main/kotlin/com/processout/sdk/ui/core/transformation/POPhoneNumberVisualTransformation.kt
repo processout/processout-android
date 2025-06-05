a8e2e2c643323174650f27d187d249ceadfd6967
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-06-05 20:49:19 +0300

    Support national/international format in POPhoneNumberVisualTransformation

--- ui-core/src/main/kotlin/com/processout/sdk/ui/core/transformation/POPhoneNumberVisualTransformation.kt
+++ ui-core/src/main/kotlin/com/processout/sdk/ui/core/transformation/POPhoneNumberVisualTransformation.kt
@@ -4,6 +4,8 @@ import android.telephony.PhoneNumberUtils
 import android.text.Selection
 import com.google.i18n.phonenumbers.PhoneNumberUtil
 import com.processout.sdk.ui.core.annotation.ProcessOutInternalApi
+import com.processout.sdk.ui.core.transformation.POPhoneNumberVisualTransformation.PhoneNumberFormat.INTERNATIONAL
+import com.processout.sdk.ui.core.transformation.POPhoneNumberVisualTransformation.PhoneNumberFormat.NATIONAL
 import java.util.Locale
 
 /** @suppress */
@@ -18,15 +20,24 @@ class POPhoneNumberVisualTransformation(
         INTERNATIONAL
     }
 
-    private val formatter = PhoneNumberUtil.getInstance().getAsYouTypeFormatter(regionCode)
+    private val util = PhoneNumberUtil.getInstance()
+    private val formatter = util.getAsYouTypeFormatter(regionCode)
 
     override fun transform(text: String): String {
         formatter.clear()
+        var dialingCode: String? = null
+        val phoneNumber = when (expectedFormat) {
+            NATIONAL -> if (regionCode.isNotBlank()) {
+                dialingCode = "+${util.getCountryCodeForRegion(regionCode)}"
+                "$dialingCode$text"
+            } else text
+            INTERNATIONAL -> text
+        }
         var formatted: String? = null
-        val cursorIndex = Selection.getSelectionEnd(text) - 1
+        val cursorIndex = Selection.getSelectionEnd(phoneNumber) - 1
         var lastNonSeparator = 0.toChar()
         var hasCursor = false
-        text.forEachIndexed { index, char ->
+        phoneNumber.forEachIndexed { index, char ->
             if (PhoneNumberUtils.isNonSeparator(char)) {
                 if (lastNonSeparator.code != 0) {
                     formatted = formatted(lastNonSeparator, hasCursor)
@@ -41,7 +52,15 @@ class POPhoneNumberVisualTransformation(
         if (lastNonSeparator.code != 0) {
             formatted = formatted(lastNonSeparator, hasCursor)
         }
-        return formatted ?: text
+        return when (expectedFormat) {
+            NATIONAL -> formatted?.let {
+                if (dialingCode != null) {
+                    val dialingCodeRegex = """^\s*(?:\+|00)?\s*\(?\s*$dialingCode\s*\)?[\s-]*""".toRegex()
+                    it.replaceFirst(dialingCodeRegex, String())
+                } else text
+            } ?: text
+            INTERNATIONAL -> formatted ?: text
+        }
     }
 
     private fun formatted(lastNonSeparator: Char, hasCursor: Boolean) =
