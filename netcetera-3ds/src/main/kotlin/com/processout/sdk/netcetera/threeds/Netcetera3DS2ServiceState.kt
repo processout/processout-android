b0e8436d1ed9e074cff6b601c9a7fafce3283da4
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-08-11 15:58:46 +0300

    Netcetera3DS2ServiceState

---
+++ netcetera-3ds/src/main/kotlin/com/processout/sdk/netcetera/threeds/Netcetera3DS2ServiceState.kt
@@ -0,0 +1,35 @@
+package com.processout.sdk.netcetera.threeds
+
+import com.netcetera.threeds.sdk.api.ThreeDS2Service
+import com.netcetera.threeds.sdk.api.transaction.Transaction
+import com.processout.sdk.netcetera.threeds.Netcetera3DS2ServiceState.Fingerprinted
+
+internal sealed interface Netcetera3DS2ServiceState {
+
+    data object Idle : Netcetera3DS2ServiceState
+
+    data class Fingerprinting(
+        val serviceContext: Netcetera3DS2ServiceContext
+    ) : Netcetera3DS2ServiceState
+
+    data class Fingerprinted(
+        val serviceContext: Netcetera3DS2ServiceContext
+    ) : Netcetera3DS2ServiceState
+
+    data class Challenging(
+        val serviceContext: Netcetera3DS2ServiceContext
+    ) : Netcetera3DS2ServiceState
+}
+
+internal data class Netcetera3DS2ServiceContext(
+    val threeDS2Service: ThreeDS2Service,
+    val transaction: Transaction
+)
+
+internal inline fun Netcetera3DS2ServiceState.whenFingerprinted(
+    crossinline block: (serviceContext: Netcetera3DS2ServiceContext) -> Unit
+) {
+    if (this is Fingerprinted) {
+        block(serviceContext)
+    }
+}
