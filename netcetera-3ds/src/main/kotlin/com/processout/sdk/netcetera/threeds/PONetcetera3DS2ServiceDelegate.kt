bc7e1b0eee1ab4a66df3d379232401a0762684e1
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-08-19 22:31:26 +0300

    PONetcetera3DS2ServiceDelegate

--- netcetera-3ds/src/main/kotlin/com/processout/sdk/netcetera/threeds/PONetcetera3DS2ServiceDelegate.kt
+++ netcetera-3ds/src/main/kotlin/com/processout/sdk/netcetera/threeds/PONetcetera3DS2ServiceDelegate.kt
@@ -1,8 +1,15 @@
 package com.processout.sdk.netcetera.threeds
 
 import androidx.activity.ComponentActivity
+import com.netcetera.threeds.sdk.api.security.Warning
 
 interface PONetcetera3DS2ServiceDelegate {
 
     fun activity(): ComponentActivity?
+
+    /**
+     *  Asks delegate whether the service should continue with the given warnings.
+     *  Default implementation ignores any warnings and returns _true_.
+     */
+    suspend fun shouldContinue(warnings: Set<Warning>): Boolean = true
 }
