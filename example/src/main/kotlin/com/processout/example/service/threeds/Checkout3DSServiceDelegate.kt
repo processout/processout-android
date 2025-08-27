df622bdad26ed4694ffa7bc97ee286a63bb8e239
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-08-27 15:53:58 +0300

    Checkout3DSServiceDelegate

--- example/src/main/kotlin/com/processout/example/service/threeds/Checkout3DSServiceDelegate.kt
+++ example/src/main/kotlin/com/processout/example/service/threeds/Checkout3DSServiceDelegate.kt
@@ -1,7 +1,7 @@
 package com.processout.example.service.threeds
 
 import android.app.Activity
-import android.net.Uri
+import androidx.core.net.toUri
 import com.checkout.threeds.domain.model.UICustomization
 import com.checkout.threeds.standalone.models.ConfigParameters
 import com.checkout.threeds.standalone.models.ThreeDS2ServiceConfiguration
@@ -17,19 +17,21 @@ class Checkout3DSServiceDelegate(
     private val returnUrl: String
 ) : POCheckout3DSServiceDelegate {
 
-    override fun configuration(parameters: ConfigParameters): ThreeDS2ServiceConfiguration {
-        return ThreeDS2ServiceConfiguration(
+    override fun configuration(parameters: ConfigParameters) =
+        ThreeDS2ServiceConfiguration(
             context = activity,
             configParameters = parameters,
             // Optional properties.
             locale = Locale.UK,
             uiCustomization = UICustomization(),
-            appUri = Uri.parse("https://my-app-url.com"),
+            appUri = "https://my-app-url.com".toUri(),
             challengeTimeout = 300
         )
-    }
 
-    override fun handle(redirect: PO3DSRedirect, callback: (ProcessOutResult<String>) -> Unit) {
+    override fun handle(
+        redirect: PO3DSRedirect,
+        callback: (ProcessOutResult<String>) -> Unit
+    ) {
         customTabLauncher.launch(redirect, returnUrl, callback)
     }
 }
