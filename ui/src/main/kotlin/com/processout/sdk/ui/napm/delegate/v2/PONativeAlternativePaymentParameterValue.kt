47c9e1f6380cd69d59a66eaba596b0a84e3f96af
Vitalii Vanziak <114916876+vitalii-vanziak-cko@users.noreply.github.com>
2025-09-01 16:06:13 +0300

    feat(POM-518): (nAPM) Support default phone number in international format (#326)

--- ui/src/main/kotlin/com/processout/sdk/ui/napm/delegate/v2/PONativeAlternativePaymentParameterValue.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/napm/delegate/v2/PONativeAlternativePaymentParameterValue.kt
@@ -1,4 +1,10 @@
+@file:Suppress("unused")
+
 package com.processout.sdk.ui.napm.delegate.v2
+
+import com.google.i18n.phonenumbers.NumberParseException
+import com.google.i18n.phonenumbers.PhoneNumberUtil
+import com.processout.sdk.core.logger.POLogger
 
 /**
  * Native alternative payment parameter value.
@@ -29,6 +35,31 @@ data class PONativeAlternativePaymentParameterValue internal constructor(
                 number = number
             )
         )
+
+        /**
+         * Phone number value.
+         *
+         * @param[internationalNumber] International phone number.
+         */
+        fun phoneNumber(internationalNumber: String): PONativeAlternativePaymentParameterValue =
+            try {
+                val phoneNumberUtil = PhoneNumberUtil.getInstance()
+                val parsedNumber = phoneNumberUtil.parse(internationalNumber, null)
+                PONativeAlternativePaymentParameterValue(
+                    Value.PhoneNumber(
+                        regionCode = phoneNumberUtil.getRegionCodeForNumber(parsedNumber) ?: String(),
+                        number = parsedNumber.nationalNumber.toString()
+                    )
+                )
+            } catch (e: NumberParseException) {
+                POLogger.debug("Failed to parse international phone number [%s]: %s", internationalNumber, e)
+                PONativeAlternativePaymentParameterValue(
+                    Value.PhoneNumber(
+                        regionCode = String(),
+                        number = String()
+                    )
+                )
+            }
     }
 
     internal sealed class Value {
