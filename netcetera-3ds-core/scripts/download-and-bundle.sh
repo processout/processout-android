6490c80216c35017cc2cf8e0ecffb8333456bdc4
Vitalii Vanziak <114916876+vitalii-vanziak-cko@users.noreply.github.com>
2025-11-11 18:29:14 +0200

    feat(POM-517): Automate publishing of Netcetera module to Maven Central via CI (#349)

---
+++ netcetera-3ds-core/scripts/download-and-bundle.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+URL="$1"
+FILENAME=$(basename "$URL")
+SDK_DIR="${FILENAME%.*}"
+
+if curl -L "$URL" -o "$FILENAME"; then
+  echo "Downloaded $FILENAME successfully."
+else
+  echo "Failed to download: $URL"
+  exit 1
+fi
+
+unzip -o "$FILENAME" -d "$SDK_DIR"
+
+# assets
+rm -rf netcetera-3ds-core/src/main/assets/*
+cp -r "$SDK_DIR"/assets/* netcetera-3ds-core/src/main/assets/
+
+# jniLibs
+rm -rf netcetera-3ds-core/src/main/jniLibs/*
+cp -r "$SDK_DIR"/jni/* netcetera-3ds-core/src/main/jniLibs/
+
+# res
+rm -rf netcetera-3ds-core/src/main/res/*
+cp -r "$SDK_DIR"/res/* netcetera-3ds-core/src/main/res/
+
+# classes.jar
+cp -f "$SDK_DIR"/classes.jar netcetera-3ds-core/libs/
+
+# proguard & dexguard
+cp -f "$SDK_DIR"/proguard.txt netcetera-3ds-core/netcetera-consumer-rules.pro
+cp -f "$SDK_DIR"/dexguard.txt netcetera-3ds-core/
+
+echo "Netcetera $FILENAME has been bundled successfully."
