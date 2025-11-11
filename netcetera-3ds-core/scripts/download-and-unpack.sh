c56ab74644611c3da56a13b889fd57009f85b0b6
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-11-11 17:14:12 +0200

    Bundle SDK files

--- netcetera-3ds-core/scripts/download-and-unpack.sh
+++ netcetera-3ds-core/scripts/download-and-unpack.sh
@@ -7,7 +7,7 @@ fi
 
 URL="$1"
 FILENAME=$(basename "$URL")
-FOLDER="${FILENAME%.*}"
+SDK_DIR="${FILENAME%.*}"
 
 if curl -L "$URL" -o "$FILENAME"; then
   echo "Downloaded $FILENAME successfully."
@@ -16,4 +16,23 @@ else
   exit 1
 fi
 
-unzip "$FILENAME" -d "$FOLDER"
+unzip -o "$FILENAME" -d "$SDK_DIR"
+
+# jniLibs
+rm -rf netcetera-3ds-core/src/main/jniLibs/*
+cp -r "$SDK_DIR"/jni/* netcetera-3ds-core/src/main/jniLibs/
+
+# assets
+rm -rf netcetera-3ds-core/src/main/assets/*
+cp -r "$SDK_DIR"/assets/* netcetera-3ds-core/src/main/assets/
+
+# res
+rm -rf netcetera-3ds-core/src/main/res/*
+cp -r "$SDK_DIR"/res/* netcetera-3ds-core/src/main/res/
+
+# classes.jar
+cp -f "$SDK_DIR"/classes.jar netcetera-3ds-core/libs/
+
+# proguard & dexguard
+cp -f "$SDK_DIR"/proguard.txt netcetera-3ds-core/netcetera-consumer-rules.pro
+cp -f "$SDK_DIR"/dexguard.txt netcetera-3ds-core/
