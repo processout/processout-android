43cbf32c5c7765721f4f004eac2e231fce06dcc5
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2025-11-11 13:25:45 +0200

    download-and-unpack.sh

---
+++ netcetera-3ds-core/scripts/download-and-unpack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+URL="$1"
+FILENAME=$(basename "$URL")
+
+if curl -L "$URL" -o "$FILENAME"; then
+  echo "Downloaded $FILENAME successfully."
+else
+  echo "Failed to download: $URL"
+  exit 1
+fi
