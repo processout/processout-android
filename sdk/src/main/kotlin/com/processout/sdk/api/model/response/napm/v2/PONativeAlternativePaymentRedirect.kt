71e99f0abf4563898857e147495ba85d232e5c76
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2026-01-06 17:21:57 +0200

    Parse redirect 'type'

--- sdk/src/main/kotlin/com/processout/sdk/api/model/response/napm/v2/PONativeAlternativePaymentRedirect.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/napm/v2/PONativeAlternativePaymentRedirect.kt
@@ -1,6 +1,9 @@
 package com.processout.sdk.api.model.response.napm.v2
 
 import android.os.Parcelable
+import com.processout.sdk.core.annotation.ProcessOutInternalApi
+import com.processout.sdk.core.util.findBy
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
@@ -9,10 +12,36 @@ import kotlinx.parcelize.Parcelize
  *
  * @param[url] Redirect URL.
  * @param[hint] A hint or description associated with the redirect URL.
+ * @param[rawType] Raw redirect type.
  */
 @Parcelize
 @JsonClass(generateAdapter = true)
 data class PONativeAlternativePaymentRedirect(
     val url: String,
-    val hint: String
-) : Parcelable
+    val hint: String,
+    @Json(name = "type")
+    val rawType: String
+) : Parcelable {
+
+    /** Redirect type. */
+    val type: Type
+        get() = Type::rawType.findBy(rawType) ?: Type.UNKNOWN
+
+    /**
+     * Redirect type.
+     */
+    enum class Type(val rawType: String) {
+        /** Web redirect. */
+        WEB("web"),
+
+        /** Deep link redirect. */
+        DEEP_LINK("deep_link"),
+
+        /**
+         * Placeholder that allows adding additional cases while staying backward compatible.
+         * __Warning:__ Do not match this case directly, use _when-else_ instead.
+         */
+        @ProcessOutInternalApi
+        UNKNOWN(String())
+    }
+}
