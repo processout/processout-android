c276426b9fb7de0e60bd03f435725677c0f8586e
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2026-01-07 14:39:44 +0200

    Parse 'confirmationRequired'

--- sdk/src/main/kotlin/com/processout/sdk/api/model/response/napm/v2/PONativeAlternativePaymentRedirect.kt
+++ sdk/src/main/kotlin/com/processout/sdk/api/model/response/napm/v2/PONativeAlternativePaymentRedirect.kt
@@ -1,6 +1,9 @@
 package com.processout.sdk.api.model.response.napm.v2
 
 import android.os.Parcelable
+import com.processout.sdk.api.model.request.napm.v2.PONativeAlternativePaymentAuthorizationRequest
+import com.processout.sdk.api.model.request.napm.v2.PONativeAlternativePaymentRedirectConfirmation
+import com.processout.sdk.api.model.request.napm.v2.PONativeAlternativePaymentTokenizationRequest
 import com.processout.sdk.core.annotation.ProcessOutInternalApi
 import com.processout.sdk.core.util.findBy
 import com.squareup.moshi.Json
@@ -13,6 +16,11 @@ import kotlinx.parcelize.Parcelize
  * @param[url] Redirect URL.
  * @param[hint] A hint or description associated with the redirect URL.
  * @param[rawType] Raw redirect type.
+ * @param[confirmationRequired] Indicates whether it is required to notify the backend if the redirection was successful
+ * by sending [PONativeAlternativePaymentRedirectConfirmation]
+ * in the [PONativeAlternativePaymentAuthorizationRequest.redirectConfirmation]
+ * or the [PONativeAlternativePaymentTokenizationRequest.redirectConfirmation],
+ * depending on the flow.
  */
 @Parcelize
 @JsonClass(generateAdapter = true)
@@ -20,7 +28,9 @@ data class PONativeAlternativePaymentRedirect(
     val url: String,
     val hint: String,
     @Json(name = "type")
-    val rawType: String
+    val rawType: String,
+    @Json(name = "confirmation_required")
+    val confirmationRequired: Boolean? // TODO: make non-nullable
 ) : Parcelable {
 
     /** Redirect type. */
