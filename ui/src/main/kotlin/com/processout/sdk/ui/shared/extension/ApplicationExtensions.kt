7f393be52c4806bd06ce0e032f6190f77adfe66c
Vitalii Vanziak <vitalii.vanziak@checkout.com>
2026-03-20 14:48:01 +0200

    openDeepLink() with package names

--- ui/src/main/kotlin/com/processout/sdk/ui/shared/extension/ApplicationExtensions.kt
+++ ui/src/main/kotlin/com/processout/sdk/ui/shared/extension/ApplicationExtensions.kt
@@ -14,14 +14,34 @@ internal fun Application.currentAppLocale(): Locale =
         ?: LocaleListCompat.getAdjustedDefault()[0]
         ?: Locale.getDefault()
 
-internal fun Application.openDeepLink(url: String): Boolean {
+internal fun Application.openDeepLink(
+    url: String,
+    packageName: String? = null
+): Boolean {
     try {
         val intent = Intent(Intent.ACTION_VIEW, url.toUri())
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        packageName?.let { intent.setPackage(it) }
         startActivity(intent)
         return true
     } catch (e: ActivityNotFoundException) {
-        POLogger.warn("Failed to open deep link [%s] with exception: %s", url, e)
+        POLogger.warn("Failed to open deep link [%s] for package [%s] with exception: %s", url, packageName, e)
         return false
+    }
+}
+
+internal fun Application.openDeepLink(
+    url: String,
+    packageNames: Set<String>?
+): Boolean {
+    if (packageNames.isNullOrEmpty()) {
+        if (url.toUri().isWeb) {
+            POLogger.warn("Refused to open app link [%s] without a package, to prevent opening in an external browser.", url)
+            return false
+        }
+        return openDeepLink(url)
+    }
+    return packageNames.any { packageName ->
+        openDeepLink(url, packageName)
     }
 }
